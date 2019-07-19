fe73a74ccf10393fb3f812f35fadff55f42e6029
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-07-19 15:41:02 -0500

    Update admin-events.js

--- commands/admin/admin-events.js
+++ commands/admin/admin-events.js
@@ -19,15 +19,14 @@ class AdminEventsCommand extends commando.Command
         .setColor(0x991919)
         .addField(":calender: __**Events Team**__ :calender:",
         "Events head admins:\n\n" +
-        "Events Leadership:\n\n" + )
-        .addField("\u200b",
+        "Events Leadership:\n\n" + 
         "• Hosting capping events.\n\n" +
         "• Maintaining the events calendar.\n\n" +
         "• Hosting 2 events (per person) per month such as: Hayley’s clue events, spotlight events, bossing events, skilling events.\n\n" +
         "• Encouraging other members to host events and supporting them whilst doing so.\n\n" +
         "• If you are primarily OSRS, host suitable events for the FC community.\n\n" +
         "• Organise and host monthly skilling competitions, share the responsibility of hosting these competitions evenly amongst the events team. (take it in turns)\n\n" +
-        "• Updating the clan notice board for the events being hosted along with the clan welcome message when joining the CC.\n\n" +)
+        "• Updating the clan notice board for the events being hosted along with the clan welcome message when joining the CC.\n\n" + )
         .setThumbnail("")
         .setFooter("Last updated")
         .setTimestamp();
