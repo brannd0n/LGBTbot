855df65486a0c1a419a7247398a1407ac175409b
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-07-19 17:39:34 -0500

    Update admin-community.js

--- commands/admin/admin-community.js
+++ commands/admin/admin-community.js
@@ -27,12 +27,12 @@ class AdminCommunityCommand extends commando.Command
         "• Inviting new members to the discord and giving everyone the correct roles\n\n" +
         "• Removing inactive members\n\n" +
         "• Make sure the discord is up to date and has all relevant and correct information.\n\n"
-        "• Should you need to make changes to discord, primarily deleting things, please confirm this with other community management admins and keep a copy/archive of things deleted. This may include emotes, channels, large chunks of text etc\n\n"
-        "• Help make announcements using the LGBoT+\n\n"
-        "• Actioning Feedback surveys when needed from the Admin/Leadership Team.\n\n"
-        "• Managing the social media accounts (Twitter/Instagram etc) in publishing posts and managing account security (e.g. update login details when admins leave/ensure only the relevant people have the login details).\n\n"
-        "• Keeping a copy of frequently used image assets within the Google doc linked in the “Helpful Links” section of #admin_handbook\n\n"
-        "• Maintaining the runeclan page for the clan.\n\n"
+        "• Should you need to make changes to discord, primarily deleting things, please confirm this with other community management admins and keep a copy/archive of things deleted. This may include emotes, channels, large chunks of text etc\n\n" +
+        "• Help make announcements using the LGBoT+\n\n" +
+        "• Actioning Feedback surveys when needed from the Admin/Leadership Team.\n\n" +
+        "• Managing the social media accounts (Twitter/Instagram etc) in publishing posts and managing account security (e.g. update login details when admins leave/ensure only the relevant people have the login details).\n\n" +
+        "• Keeping a copy of frequently used image assets within the Google doc linked in the “Helpful Links” section of #admin_handbook\n\n" +
+        "• Maintaining the runeclan page for the clan.\n\n" +
         "Click here to view the clan recruitment\n\n" + 
         "Form.\n\n" )
         .setThumbnail("")
