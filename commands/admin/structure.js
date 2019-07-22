be887085b13a6c1c78c9f5d32da8ebf642ed52f9
kalmasri123 <kalmasri@me.com>
2019-07-22 19:07:26 -0400

    Update structure.js

--- commands/admin/structure.js
+++ commands/admin/structure.js
@@ -1,1 +1,25 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js');
 
+class StructureCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'structure',
+            group: 'admin',
+            memberName: 'structure',
+            description: 'Recruiting on the forums'
+        });
+    }
+
+    async run(message, args)
+    {
+
+        message.channel.send({
+  files: ['https://i.ibb.co/0fQQvHd/Screenshot-54-1.png']
+})
+    }
+}
+
+module.exports = StructureCommand;
