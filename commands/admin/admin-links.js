6c74d9a073ac6cfc55625c6257aa1595555a1234
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-07-31 23:57:56 -0500

    Update admin-links.js

--- commands/admin/admin-links.js
+++ commands/admin/admin-links.js
@@ -19,10 +19,12 @@ class AdminLinksCommand extends commando.Command
         .setColor(0x991919)
         .addField("🔗 __**HELPFUL LINKS**__ 🔗",
         "[Click here to visit the forum thread](http://services.runescape.com/m=forum/c=w9VNiocRUOQ/sl=0/forums.ws?290,291,31,66034838)\n\n"+
+        "[Click here for OSRS FC forum thread](http://services.runescape.com/m=forum/c=I4WKxSmKkbU/forums.ws?320,321,327,66099564)\n\n"+
         "[Click here to view the warning spreadsheet](https://docs.google.com/spreadsheets/d/19y9dEi98kO0loHWhzZpe0a-fPSa1CXZ8b4p_PYC7L7g)\n\n"+
         "[Click here for clan calendar](https://docs.google.com/spreadsheets/d/1voAMbOib3-6r0wB_e7eVTS2mWM-lFXSTujLaKdhh8o4/edit#gid=0)\n\n"+
         "[Click here for promotions spreadsheet](https://docs.google.com/spreadsheets/d/1UWg0trdQQJ2vSdQct9-30gTsIvO_3vLKcavD_G_QLyc/edit#gid=27995968)\n\n"+
         "[Click here for clan twitter](https://twitter.com/TheLGBTCorner)\n\n"+
+        "[Click here for OSRS FC twitter](https://twitter.com/OSRS_LGBT)\n\n"+
         "[Click here for clan facebook](https://www.facebook.com/LGBTCornerClan/)\n\n"+
         "[Click here for image resources](https://drive.google.com/drive/folders/1DcAYqV-PNgKpVonMWEsV4SpnFvjRDeJw)\n\n")
         .setThumbnail("https://www.smashbros.com/wiiu-3ds/images/character/toon_link/main.png")
