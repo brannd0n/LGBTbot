f0f2d2b7512d4939a97a44c407c7e68b1f66d7dc
Matthew Denaburg <matthew.denaburg1@gmail.com>
2019-08-07 19:04:39 -0400

    created a generic event and krilv2 event to test with

---
+++ commands/events/GenericEvent.js
@@ -0,0 +1,82 @@
+﻿const commando = require("discord.js-commando");
+
+/**
+ * A `GenericEvent`, the base class for all events used in TheLGBoT+ discord bot.
+ */
+class GenericEvent extends commando.Command {
+
+	/**
+	 * 
+	 * @param {any} client
+	 * @param {Object} cmd_info
+	 */
+    constructor(client, cmd_info, embed_info) {
+        super(client, {
+            name: cmd_info.name,
+            group: cmd_info.group,
+            memberName: cmd_info.memberName,
+            description: cmd_info.description,
+            examples: cmd_info.examples,
+            args: [
+                {
+                    key: "date",
+                    prompt: "What date is the event?",
+                    type: "string"
+                },
+                {
+                    key: "time",
+                    prompt: "What time is the event (game time)?",
+                    type: "string"
+                },
+                {
+                    key: "world",
+                    prompt: "What world is the event going to take place on? (default w23)",
+                    type: "string",
+                    default: "23"
+                }
+            ]
+        });
+
+        this.embed_info = embed_info;
+    }
+
+    async run(message, args) {
+        message.delete();
+
+        const mainEmbed = {
+            color: 0x00AE86,
+            title: this.embed_info.title,
+            author: {
+                name: message.author,
+                icon_url: message.author.displayAvatarURL
+            },
+            thumbnail: this.embed_info.thumbnail,
+            fields: [
+                {
+                    name: "📅 Date:",
+                    value: args.date,
+                    inline: true
+                },
+                {
+                    name: "🕘 Time",
+                    value: args.time + "game time",
+                    inline: true
+                },
+                {
+                    name: "🌍 World",
+                    value: args.world,
+                    inline: true
+                },
+                {
+                    name: "Host",
+                    value: message.author,
+                    inline: true
+                }
+            ]
+        };
+
+        Object.assign(mainEmbed, this.embed_info);
+
+        message.channel.sendEmbed({ embed: mainEmbed });
+    }
+}
---
+++ commands/events/krilv2.js
@@ -0,0 +1,23 @@
+﻿const generic_event = require("GenericEvent.js");
+
+
+class KrilV2 extends generic_event.GenericEvent {
+
+    constructor(client) {
+        super(client,
+            {
+                name: 'krilv2',
+                group: 'events',
+                memberName: 'kril',
+                description: 'Normal Mode Kril event details'
+            }, {
+                title: "⚔️ __**K'ril MASS**__ ⚔️",
+                thumbnail: "https://runescape.wiki/images/2/2f/K%27ril_Tsutsaroth.png"
+            }
+        );
+    }
+
+    run(message, args) {
+        super.run(message, args);
+    };
+}
