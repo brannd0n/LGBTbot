769075ceda2e9c1cc8c343feb2c65684c543b129
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-08-19 13:55:04 -0500

    Update ed1.js

--- commands/events/ed1.js
+++ commands/events/ed1.js
@@ -31,10 +31,10 @@ class Ed1Command extends commando.Command
         var myInfo = new discord.RichEmbed()
         .setTitle("⚔️ __**Temple of Aminishi**__ ⚔️")
         .setColor(0x00AE86)
-        .setFooter("Please remember that this is completely for fun! Please feel free to PM me. All loot will be traded to me and split equally between attendees.", "https://runescape.wiki/images/e/ea/Seiryu_the_Azure_Serpent.png?932ac")
+        .setFooter("Please remember that this is completely for fun! Please feel free to PM me. All loot will be traded to me and split equally between attendees.", "http://www.simpleimageresizer.com/_uploads/photos/6e756b50/Seiryu_the_Azure_Serpent1_1_optimized.png")
         .setThumbnail("https://runescape.wiki/images/e/ea/Seiryu_the_Azure_Serpent.png?932ac")
         .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + message.author)
-        .addField("\u200b", "[Strategies for Dragonkin Laboratory](https://runescape.wiki/w/Dragonkin_Laboratory/Strategies)")
+        .addField("\u200b", "[Strategies for Temple of Aminishi](https://runescape.wiki/w/Temple_of_Aminishi/Strategies)")
         .addField("\u200b", "**Requirements:**\nAt least Tier 80+ combat stats \nTier 80+ armour & weapons \nDevotion", true)
         .addField("\u200b", "**Recommended:**\nPrayer renewal \nOverloads \nSuper restore flasks \nRange or Magic Style", true)
         message.channel.sendEmbed(myInfo);
