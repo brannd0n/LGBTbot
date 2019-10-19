2c4b773b6dcfe5d3fa123143ddfa62293021a892
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-10-19 15:49:45 -0500

    Update DivRoles.js

--- commands/welcome/DivRoles.js
+++ commands/welcome/DivRoles.js
@@ -17,7 +17,7 @@ class DivRolesCommand extends commando.Command
     {
         var myInfo = new discord.RichEmbed()
         .addField("__Roles__", "While in this server everyone has the ability to self assign pronouns, you will be able to do this as soon as you join the server, we request everyone respects each other’s pronouns and hope you take full advantage of this ability.\n\n" +
-        "To assign yourself pronouns use the following command/commands in #introductions\n\n" +
+        "To assign yourself pronouns use the following command/commands in <#630747807577538570>\n\n" +
         "`!They` - <@&630739941579816967>\n" +
         "`!She` - <@&630740361337372692>\n" +
         "`!He` - <@&630740555143315463>")
