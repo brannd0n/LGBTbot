d4cf7e73408bff7d27912262b816b3a1dae09743
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-10-19 10:24:40 -0500

    Update DivWelcome.js

--- commands/welcome/DivWelcome.js
+++ commands/welcome/DivWelcome.js
@@ -16,7 +16,7 @@ class DivWelcomeCommand extends commando.Command
     async run(message, args)
     {
         var myInfo = new discord.RichEmbed()
-        .setTitle(":emoji_1: __**WELCOME TO THE OFFICIAL RUNESCAPE DIVERSITY DISCORD SERVER**__ :emoji_1:")
+        .setTitle("<:emoji_1:> __**WELCOME TO THE OFFICIAL RUNESCAPE DIVERSITY DISCORD SERVER**__ <:emoji_1:>")
         .setColor(0xFF0000)
         .setDescription("Welcome to the Runescape Diversity & Inclusion Discord, player run and Jagex Mod supported. This discord was brought about after a meeting between players and Jagex Diversity & Inclusion (D&I)  Mods at Runefest 2019 regarding diversity within the game.")
         .setThumbnail("https://media0.giphy.com/media/l378zKVk7Eh3yHoJi/giphy.gif")
