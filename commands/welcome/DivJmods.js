1f886721014c56d95249e6e64c09be482ae0f1d6
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-10-19 17:28:57 -0500

    Update DivJmods.js

--- commands/welcome/DivJmods.js
+++ commands/welcome/DivJmods.js
@@ -17,7 +17,7 @@ class DivJmodsCommand extends commando.Command
     {
         var myInfo = new discord.RichEmbed()
         .setColor(0x00FFFF)
-        .addField("__Diversity within Jagex__", "Diversity at Jagex is very broad and includes Jmods from varying departments, **although not everyone from the teams will be in this discord,** here is a list of some of the teams that are a part of the diversity group.\n\n" +
+        .addField("__Diversity within Jagex__", "Diversity at Jagex is very broad and includes Jmods from varying departments, __**although not everyone from the teams will be in this discord,**__ here is a list of some of the teams that are a part of the diversity group.\n\n" +
         "Analytics & Data Science\n" +
         "Audio\n" +
         "Communications\n" +
