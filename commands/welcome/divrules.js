0bfcfb3e0c89ae8f320472cd90fcb8999b45bff7
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-10-18 19:42:40 -0500

    Update divrules.js

--- commands/welcome/divrules.js
+++ commands/welcome/divrules.js
@@ -18,7 +18,7 @@ class DivRulesCommand extends commando.Command
         var myInfo = new discord.RichEmbed()
         .setColor(0xFD6600)
         .setTimestamp()
-        .addField(" __**SERVER RULES**__", "**1)** Any form of hate speech or abusive behaviour will not be tolerated and will result in immediate removal from the server. \n \n**2)** Nothing NSFW to be posted ANYWHERE in the server. \n \n **3)** Please respect each other’s pronouns and try your best to use them correctly. \n \n **4)** Some sensitive topics are allowed to be discussed in this server, however please avoid topics of self harm or suicide.\n **5)** Please set your nickname to include rsn to avoid confusion.", true)
+        .addField(" __**SERVER RULES**__", "\n **1)** Any form of hate speech or abusive behaviour will not be tolerated and will result in immediate removal from the server. \n \n**2)** Nothing NSFW to be posted ANYWHERE in the server. \n \n **3)** Please respect each other’s pronouns and try your best to use them correctly. \n \n **4)** Some sensitive topics are allowed to be discussed in this server, however please avoid topics of self harm or suicide.\n \n **5)** Please set your nickname to include rsn to avoid confusion.", true)
         .setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/228/scroll_1f4dc.png")      
         .setFooter("Last updated")
         .setTimestamp()
