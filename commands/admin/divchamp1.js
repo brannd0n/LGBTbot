addecb6cea9b5879b76980681e928bb60a4cbf68
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-10-19 17:47:11 -0500

    Update divchamp1.js

--- commands/admin/divchamp1.js
+++ commands/admin/divchamp1.js
@@ -17,7 +17,7 @@ class DivChamp1Command extends commando.Command
     {
         var myInfo = new discord.RichEmbed()
         .setColor(0x991919)
-        .addField("📋 __**Diversity Champion Handbook**__ 📋", "First of all we would like to thank you all for helping with this community, diversity champions are here to moderate this discord and ensure it remains a safe space, as such we have a few simple rules and guidelines we ask everyone who joins to follow.\n\n" +
+        .addField(":clipboard: __**Diversity Champion Handbook**__ :clipboard:", "First of all we would like to thank you all for helping with this community, diversity champions are here to moderate this discord and ensure it remains a safe space, as such we have a few simple rules and guidelines we ask everyone who joins to follow.\n\n" +
         "Those of you who are diversity champions have been selected to join the team because you are an advocate for diversity issues and are keen to see positive changes, we feel you can really help to improve the community.")
         .setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/lg/57/books_1f4da.png")
         .setFooter("Last updated")
