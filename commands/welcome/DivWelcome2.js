531ce6d79ddeeda1c62760291ff37cc1cffc0cfa
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-10-19 13:10:02 -0500

    Update DivWelcome2.js

--- commands/welcome/DivWelcome2.js
+++ commands/welcome/DivWelcome2.js
@@ -19,8 +19,7 @@ class DivWelcome2Command extends commando.Command
         .addField("\u200b", "The aim of the discord is to create a safe space for players to talk about aspects of the game & community which could be improved to make people feel more included." +
         " Examples of this are suggestions for more NPCs in game which represent how you identify, raising accessibility issues or bringing situations to our attention which you found alienating or hurtful." +
         " We cannot guarantee that all suggestions will make it to the game, but we aim to implement them where possible or find compromises." +
-        " We want to enable open conversations and create better mutual understanding between JMods and players.")
-        .addBlankField()
+        " We want to enable open conversations and create better mutual understanding between JMods and players.\n\n")
         .addField("\u200b", "This server has been endorsed by RuneScape community management and JMods within the discord can clearly be identified by the “JMod” role. Please be aware to not excessively tag as their involvement is in their own personal time.")
         .setColor(0x00FF00)
         .setFooter("Last updated")
