315c5d6957c589eac87930488b2f733d5cf1d3a6
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-10-25 11:31:39 -0500

    Update divmission.js

--- commands/welcome/divmission.js
+++ commands/welcome/divmission.js
@@ -18,11 +18,10 @@ class DivMissionCommand extends commando.Command
         var myInfo = new discord.RichEmbed()
         .setColor(0xFF00D6)
         .setTimestamp()
-        .addField(" __Mission Statement__", "The purpose of Diversity at Jagex is to promote inclusivity and equal opportunities within the company. Each individual, each interest group, each background is important.\n\n" +
-        " As such, this is a support network for those who feel they would benefit from this support, those who want to promote positive change, or those who are allies.\n\n" +
-        "(Originally, this was Women at Jagex but our remit is essentially to ensure that no one is erased, disadvantaged or discriminated against.) Anyone who values the principles of tolerance, fairness and openness is welcome!",true)
+        .addField(" __About Us__", "The purpose of Diversity at Jagex is to promote inclusivity and equal opportunities within the company and without. It functions as a support network, a framework to create positive change, and a place for allies to show their support.\n\n" +
+        "Our remit is to ensure that no one is erased, disadvantaged or discriminated against. Anyone who values the principles of tolerance, fairness and openness is welcome!\n\n" ,true)
         .addBlankField()
-        .addField(" __Sub Aims:__", "**-** Inspire people from underrepresented groups to apply to Jagex, by various means including outreach & mentorship from and increased visibility of Jagex employees from underrepresented groups, support of Jagex PR to portray Jagex as an inclusive company, support of recruitment by attending conferences and sponsoring relevant events. \n\n**-** Improve retention of employees from underrepresented groups by targeted mentorships, career support, recognition of contribution. \n\n**-** Improve corporate social strategy & culture by building awareness for the benefits of inclusivity within the company and our games, finding allies and organising educational events.\n\n", true)
+        .addField(" __Our Goals__", "**•** To inspire people from underrepresented groups to apply to Jagex by making ourselves visible at conferences and events, providing mentorship, and developing an inclusive approach to hiring and graduate schemes. \n\n**•** To raise awareness of the benefits of inclusivity in our games by organising educational events and reaching out to allies. \n\n**•** To support our diverse community of players by creating safe spaces for communication, increasing diverse representation within our game and sponsoring relevant events.\n\n", true)
         .setThumbnail("https://media.giphy.com/media/SwyZW2d6x3KMlCeE1O/giphy.gif")
         .setFooter("Last updated")
         .setTimestamp()
