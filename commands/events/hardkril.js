05b6859020a3f1264274ff8330f858ad6c2031e6
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-10-26 12:36:03 -0500

    Update hardkril.js

--- commands/events/hardkril.js
+++ commands/events/hardkril.js
@@ -1,15 +1,15 @@
 const commando = require('discord.js-commando');
 const discord = require('discord.js');
 
-class HardkrilCommand extends commando.Command
+class HardzilyCommand extends commando.Command
 {
     constructor(client)
     {
         super(client,{
-            name: 'hardkril',
+            name: 'hardzily',
             group: 'events',
-            memberName: 'hardkril',
-            description: 'Hard Mode Kril event details',
+            memberName: 'hardzily',
+            description: 'Hard Mode Zilyana event details',
             args: [
                 {
                     key: 'date',
@@ -34,17 +34,17 @@ class HardkrilCommand extends commando.Command
     {
         message.delete();
         var myInfo = new discord.RichEmbed()
-        .setTitle("⚔️ __**HARDMODE K'ril MASS**__ ⚔️")
+        .setTitle("⚔️ __**HARDMODE Zilyana MASS**__ ⚔️")
         .setColor(0x00AE86)
-        .setFooter("Please remember that this is completely for fun! Just hit K'ril and avoid the specials. That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/2/2f/K%27ril_Tsutsaroth.png")
-        .setThumbnail("https://runescape.wiki/images/2/2f/K%27ril_Tsutsaroth.png")
+        .setFooter("Please remember that this is completely for fun! Just hit Zily and avoid her attacks. She has 2 phases in hardmode and must be finished off with an Ultimate ability that deals damage! That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/f/fb/Commander_Zilyana.png?4fd88")
+        .setThumbnail("https://runescape.wiki/images/f/fb/Commander_Zilyana.png?4fd88")
         .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + message.author)
-        .addField("\u200b", "[Hard mode strategies for K'ril Tsutsaroth](https://runescape.wiki/w/K%27ril_Tsutsaroth/Strategies/Hard_mode)")
-        .addField("\u200b", "**Requirements:**\nTroll Stronghold \n70 Constutution", true)
+        .addField("\u200b", "[Hard mode strategies for Commander Zilyana](https://runescape.wiki/w/Commander_Zilyana/Strategies/Hard_mode)")
+        .addField("\u200b", "**Requirements:**\nTroll Stronghold \n70 Agility", true)
         .addField("\u200b", "**Recommended:**\nTier 70 Armour or higher \n Overloads \n Soulsplit", true)
         .addField("\u200b", args.description, true)
         message.channel.sendEmbed(myInfo);
     }
 }
 
-module.exports = HardkrilCommand;
+module.exports = HardzilyCommand;
