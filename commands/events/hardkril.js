aac4f149c241c85c11d40f91721e4c48e2cdb950
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-10-26 12:33:15 -0500

    Update hardkril.js

--- commands/events/hardkril.js
+++ commands/events/hardkril.js
@@ -20,10 +20,15 @@ class HardkrilCommand extends commando.Command
                     key: 'time',
                     prompt: 'What time is the event? (e.g. 21:00)',
                     type: 'string'
-                }
-            ]
-        });
-    }
+                  },
+                  {
+                      key: 'description',
+                      prompt: 'Describe the event',
+                      type: 'string'
+                  }
+              ]
+          });
+      }
 
     async run(message, args)
     {
@@ -37,6 +42,7 @@ class HardkrilCommand extends commando.Command
         .addField("\u200b", "[Hard mode strategies for K'ril Tsutsaroth](https://runescape.wiki/w/K%27ril_Tsutsaroth/Strategies/Hard_mode)")
         .addField("\u200b", "**Requirements:**\nTroll Stronghold \n70 Constutution", true)
         .addField("\u200b", "**Recommended:**\nTier 70 Armour or higher \n Overloads \n Soulsplit", true)
+        .addField("\u200b", args.description, true)
         message.channel.sendEmbed(myInfo);
     }
 }
