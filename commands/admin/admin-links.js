56ffd890093d3d6cc72c001811fba2bd900dde30
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-11-17 13:31:23 -0600

    Update admin-links.js

--- commands/admin/admin-links.js
+++ commands/admin/admin-links.js
@@ -20,8 +20,8 @@ class AdminLinksCommand extends commando.Command
         .addField("🔗 __**HELPFUL LINKS**__ 🔗",
         "[Click here to visit the clan forum thread](http://services.runescape.com/m=forum/c=WfmoI4xdajU/forums.ws?290,291,452,66119156,goto,1)\n\n"+
         "[Click here to visit the recruitment forum thread](http://services.runescape.com/m=forum/c=mceHK8S1nWA/sl=0/forums.ws?94,95,goto,1)\n\n"+
-        "[Click here to view the warning spreadsheet](https://docs.google.com/spreadsheets/d/19y9dEi98kO0loHWhzZpe0a-fPSa1CXZ8b4p_PYC7L7g)\n\n"+
-        "[Click here for clan calendar](https://docs.google.com/spreadsheets/d/19sW_GnhAl-EqepFAZxp69ZyxiSfMAzmBUeh-xyM9hSc/edit?usp=sharing)\n\n"+
+        "[Click here to view the warning spreadsheet](https://docs.google.com/spreadsheets/d/1PCatwsY5JnrsBl01Z8srx8d2xr0gyrfb-BS7NXy4iXs/edit?usp=sharing)\n\n"+
+        "[Click here for clan calendar](https://docs.google.com/spreadsheets/d/1PCatwsY5JnrsBl01Z8srx8d2xr0gyrfb-BS7NXy4iXs/edit?usp=sharing)\n\n"+
         "[Click here for promotions spreadsheet](https://docs.google.com/spreadsheets/d/1zPtnid4-boHb04PXO8QNaohRkMQGaMp2Gs5w8_pgNHA/edit?usp=sharing)\n\n"+
         "[Click here for clan twitter](https://twitter.com/TheLGBTCorner)\n\n"+
         "[Click here for clan facebook](https://www.facebook.com/LGBTCornerClan/)\n\n"+
