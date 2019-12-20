71636861f27d1dbec4dc170d11adbcbe06181c5e
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-12-20 01:40:30 -0600

    Update divfaq.js

--- commands/welcome/divfaq.js
+++ commands/welcome/divfaq.js
@@ -19,13 +19,13 @@ class DivFaqCommand extends commando.Command
         .setColor(0xFD6600)
         .setTimestamp()
         .setTitle("__FAQ__")
-        .addField("**When will I become a verified member?**",
+        .addField("__**When will I become a verified member?**__",
         "To unlock the rest of the server you need to achieve level 3, to do this all you have to do is talk and get to know the other server members for approx 30/40 minutes give or take, once you have achieved level 3 <@159985870458322944> will pm you and a <@&630489074360123392> will assign the verified role when they are available - you don’t need to pm them they get a notification too.", true)
-        .addField("**My friend is interested in joining the Discord, how can I get an invite for them?**",
+        .addField("__**My friend is interested in joining the Discord, how can I get an invite for them?**__",
         "Please feel free to contact any <@&630489074360123392> for further details.", true)
-        .addField("**I'm unsure if my comment/language will offend someone?**",
+        .addField("__**I'm unsure if my comment/language will offend someone?**__",
         "Please feel free to run this by any <@&630489074360123392>. Alternatively you can also research this on the internet.", true)
-        .addField("**I have a suggestion for the server, where can I post this?**",
+        .addField("__**I have a suggestion for the server, where can I post this?**__",
         "We welcome any suggestions you may have, you can either DM a Diversity Champion or tag them with your suggestion in the relevant channel or in <#630461746070421515>.", true)
         .setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/softbank/145/black-question-mark-ornament_2753.png")
         .setFooter("Last updated")
