f8705dc08c93cace73c6c2a76c9b5bb8628278e4
brannd0n <52438429+brannd0n@users.noreply.github.com>
2019-12-20 13:58:07 -0600

    Update divchamp2.js

--- commands/admin/divchamp2.js
+++ commands/admin/divchamp2.js
@@ -23,8 +23,8 @@ class DivChamp2Command extends commando.Command
         "Please keep an eye on <#630747807577538570> for new members, once they have filled out all the information you will see a ticket at the top of the server, read through the information and when you are happy you can assign them the <@&630765616697049088> or <@&630765530030014476> roles (remember to check the forums to see that they have posted). \n\n" +
         "You will then need to manually assign the <@&639459046340165652> role this will unlock our more social rooms such as <#630461746070421515>, <#639434368963837953> and <#638367697519902730>. Then click on the :x: to close the ticket.\n\n"+
         "Once the <@&639459046340165652> role has been assigned the bot <@159985870458322944> will begin to track how many messages each individual has sent, after a roughly 30 minute conversation (25+ messages) you will see a notification in #Probation that an individual has achieved level 3, at this point you can assign the <@&642636421185929218> role and it will unlock the rest of the server.\n\n"+
-        "PLEASE REMEMBER TO READ WHAT EACH INDIVIDUAL HAS BEEN SAYING BEFORE VERIFYING.\n \n"+
-        "Due to the amount of trolls we received when we first opened the server this is a way for us to get to know our members better without locking the server completely, after 25+ messages we should know a person's interests well enough to determine if they are here to troll or not.")
+        "PLEASE REMEMBER TO READ WHAT EACH INDIVIDUAL HAS BEEN SAYING BEFORE VERIFYING.\n \n")
+        .addField("\200b", "Due to the amount of trolls we received when we first opened the server this is a way for us to get to know our members better without locking the server completely, after 25+ messages we should know a person's interests well enough to determine if they are here to troll or not.")
         .setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/whatsapp/224/old-key_1f5dd.png")
         .setFooter("Last updated")
         .setTimestamp();
