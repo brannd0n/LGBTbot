2d56264144321575f9cc1c3bec3243a81319d7da
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-01-09 10:20:47 -0600

    Update clannieheist.js

--- commands/events/clannieheist.js
+++ commands/events/clannieheist.js
@@ -26,7 +26,7 @@ class ClannieHeistCommand extends commando.Command
                     key: 'host',
                     prompt: 'Who is the host?',
                     type: 'string'
-                },
+                }
             ]
         });
     }
