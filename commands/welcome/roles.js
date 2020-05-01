b7b5197b36804ebfccb2017bd2ad79a6a8a71fe0
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-05-01 16:10:24 -0500

    Add files via upload

---
+++ commands/welcome/roles.js
@@ -0,0 +1,29 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js');
+
+class RolesCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'roles',
+            group: 'welcome',
+            memberName: 'roles',
+            description: 'Discord roles.'
+        });
+    }
+
+    async run(message, args)
+    {
+        var myInfo = new discord.RichEmbed()
+        .setColor(0x22FF00)
+        .setFooter("Last updated")
+        .setTimestamp()
+        .addField("🔡 __Discord Roles__ 🔡", "While you are in the discord you also\n have the option to choose roles, to\n represent how you identify.\n \n We have tried to be as inclusive as\n possible with the roles that are\n available, however if the role you want\n isn't on the current list please let a\n member of the <@&601391497698344960> team know and\n we will add it as long as it's **within reason**.", true)
+        .addField("\u200b", "These are the current roles available, for everybody to choose:\n __**PLEASE ASK A MEMBER OF THE ADMIN TEAM IF YOU WANT ANY**__")
+        .setImage("https://media.discordapp.net/attachments/261530348310102019/520759726867480577/unknown.png");
+        message.channel.sendEmbed(myInfo);
+    }
+}
+
+module.exports = RolesCommand;
