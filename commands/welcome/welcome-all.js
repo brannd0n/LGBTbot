b4fe351dc4f98acbb838123cb7191f46cd62fca0
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-05-01 16:10:57 -0500

    Add files via upload

---
+++ commands/welcome/welcome-all.js
@@ -0,0 +1,49 @@
+const commando = require('discord.js-commando');
+const WelcomeInfoCommand = require('./welcome.js');
+const RulesCommand = require('./rules.js');
+const RanksCommand = require('./ranks.js');
+const OsrsranksCommand = require('./osrsranks.js');
+const RolesCommand = require('./roles.js');
+const EventRolesCommand = require('./Eventroles.js');
+const CitadelCommand = require('./citadel.js');
+const SocialMediaCommand = require('./social-media.js');
+const CalendarCommand = require('./calendar.js');
+var welcome, rules, ranks, osrsranks, roles, eventroles, citadel, socialmedia, calendar;
+
+class WelcomeAllCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'welcome-all',
+            group: 'welcome',
+            memberName: 'welcome-all',
+            description: 'Does all welcome messages'
+        });
+        welcome = new WelcomeInfoCommand(client);
+        rules = new RulesCommand(client);
+        ranks = new RanksCommand(client);
+        osrsranks = new OsrsanksCommand(client);
+        roles = new RolesCommand(client);
+        eventroles = new RolesCommand(client);
+        citadel = new CitadelCommand(client);
+        socialmedia = new SocialMediaCommand(client);
+        calendar = new CalendarCommand(client);
+    }
+
+    async run(message, args)
+    {
+        message.delete();
+        welcome.run(message)
+            .then(rules.run(message))
+            .then(ranks.run(message))
+            .then(osrsranks.run(message))
+            .then(roles.run(message))
+            .then(eventroles.run(message))
+            .then(citadel.run(message))
+            .then(socialmedia.run(message))
+            .then(calendar.run(message));
+    }
+}
+
+module.exports = WelcomeAllCommand;
