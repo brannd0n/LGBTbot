5da2440c0088f1a2dd30635317b7f460d714d2fa
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-05-05 20:53:58 -0500

    Update osrsmole.js

--- commands/events/osrsmole.js
+++ commands/events/osrsmole.js
@@ -40,9 +40,10 @@ class OsrsmoleCommand extends commando.Command
         .setFooter("Please remember that this is completely for fun! Just follow the mole and hit her. That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://oldschool.runescape.wiki/images/f/fd/Giant_Mole.png?3f58a")
         .setThumbnail("https://oldschool.runescape.wiki/images/f/fd/Giant_Mole.png?3f58a")
         .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 523\n**Host:** " + message.author)
-        .addField("\u200b", "[Strategies for Giant mole](https://runescape.wiki/w/Giant_mole/Strategies)")
+        .addField("\u200b", "[Strategies for Giant mole](https://oldschool.runescape.wiki/w/Giant_Mole/Strategies)")
         .addField("\u200b", "**Requirements:**\nLantarn or Firecape \nSpade", true)
         .addField("\u200b", "**Recommended:**\nFood \nPrayer Potions", true)
+        .addBlankField()
         .addField("\u200b", args.description, true)
         message.channel.sendEmbed(myInfo);
     }
