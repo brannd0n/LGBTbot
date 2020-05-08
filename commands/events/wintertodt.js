98fb0a9cd6a0d4ba5bcf1f448e8df9bee0ca8a0f
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-05-07 20:48:24 -0500

    Add files via upload

---
+++ commands/events/wintertodt.js
@@ -0,0 +1,52 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js');
+
+class OsrswintertodtCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'osrswintertodt',
+            group: 'events',
+            memberName: 'osrswintertodt',
+            description: 'Wintertodt event details',
+            args: [
+                {
+                    key: 'date',
+                    prompt: 'What date is the event? (e.g. Friday 28 September)',
+                    type: 'string'
+                },
+                {
+                    key: 'time',
+                    prompt: 'What time is the event? (e.g. 21:00)',
+                    type: 'string'
+                  },
+                  {
+                      key: 'description',
+                      prompt: 'Describe the event',
+                      type: 'string'
+                  }
+              ]
+          });
+      }
+
+
+    async run(message, args)
+    {
+        message.delete();
+        var myInfo = new discord.RichEmbed()
+        .setTitle(":fire:__**Wintertodt Event**__:fire:")
+        .setColor(0x00AE86)
+        .setFooter("Please remember that this is completely for fun!", "https://oldschool.runescape.wiki/images/7/78/Burning_brazier_%28Wintertodt%29.png?7b131")
+        .setThumbnail("https://oldschool.runescape.wiki/images/7/78/Burning_brazier_%28Wintertodt%29.png?7b131")
+        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 523\n**Host:** " + message.author)
+        .addField("\u200b", "[Strategies for Wintertodt](https://oldschool.runescape.wiki/w/Wintertodt/Strategies)")
+        .addField("\u200b", "**Requirements:**\nAxe \nLevel 50 Firemaking \nWarm Clothing [List of Warm Cloting] (https://oldschool.runescape.wiki/w/Wintertodt/Warm_clothing)) ", true)
+        .addField("\u200b", "**Recommended:**\nFood", true)
+        .addBlankField()
+        .addField("\u200b", args.description, true)
+        message.channel.sendEmbed(myInfo);
+    }
+}
+
+module.exports = OsrswintertodtCommand;
