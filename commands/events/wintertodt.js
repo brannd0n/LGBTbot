6c34cb36528c9e6f7256a7c8f59c1f4bf9ac52ef
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-05-07 20:57:55 -0500

    Update wintertodt.js

--- commands/events/wintertodt.js
+++ commands/events/wintertodt.js
@@ -35,13 +35,13 @@ class WintertodtCommand extends commando.Command
     {
         message.delete();
         var myInfo = new discord.RichEmbed()
-        .setTitle(":fire:__**Wintertodt Event**__:fire:")
+        .setTitle(":fire: __**Wintertodt Event**__ :fire:")
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun!", "https://oldschool.runescape.wiki/images/7/78/Burning_brazier_%28Wintertodt%29.png?7b131")
         .setThumbnail("https://oldschool.runescape.wiki/images/7/78/Burning_brazier_%28Wintertodt%29.png?7b131")
         .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + "\n🌍 **World:** 523\n**Host:** " + message.author)
         .addField("\u200b", "[Strategies for Wintertodt](https://oldschool.runescape.wiki/w/Wintertodt/Strategies)")
-        .addField("\u200b", "**Requirements:**\nAxe \nLevel 50 Firemaking \nWarm Clothing [List of Warm Cloting] (https://oldschool.runescape.wiki/w/Wintertodt/Warm_clothing)", true)
+        .addField("\u200b", "**Requirements:**\nAxe \nLevel 50 Firemaking \nWarm Clothing [List of Warm Cloting](https://oldschool.runescape.wiki/w/Wintertodt/Warm_clothing)", true)
         .addField("\u200b", "**Recommended:**\nFood", true)
         .addBlankField()
         .addField("\u200b", args.description, true)
