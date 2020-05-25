fc6b5581d9ef312189ef92f6d1cf1b3d2338d143
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-05-25 01:28:25 -0500

    Update osrsdk.js

--- commands/events/osrsdk.js
+++ commands/events/osrsdk.js
@@ -42,7 +42,7 @@ class OsrsdkCommand extends commando.Command
         .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 523\n**Host:** " + message.author)
         .addField("\u200b", "[Strategies for Daganoth Kings](https://oldschool.runescape.wiki/w/Dagannoth_Kings/Strategies)")
         .addField("\u200b", "**Requirements:**\nFremninik Trials or 1k gp for boat to Waterbirth ", true)
-        .addField("\u200b", "**Recommended:**\n<:Attack:714342791546667180> 70+ \n:strength: 70+ \n:magic: 75+ \n:range: 70+ \n:defence: 80+ \n:Hp: 70+ \n:prayer: 45+", true)
+        .addField("\u200b", "**Recommended:**\n<:Attack:714342791546667180> 70+ \n<:Strength:714343707125612554> 70+ \n<:Magic:714343927771299840> 75+ \n<:Ranged:714343998432477205> 70+ \n<:Defence:714344066669609032> 80+ \n<:Hp:714344209632591872> 70+ \n<:Prayer:714343809433075722> 45+", true)
         .addBlankField()
         .addField("\u200b", args.description, true)
         message.channel.sendEmbed(myInfo);
