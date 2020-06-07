c89eb720514bd30a5f34c397e5d34b4815eecbc9
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-06-07 18:36:16 -0500

    Update osrsranks.js

--- commands/welcome/osrsranks.js
+++ commands/welcome/osrsranks.js
@@ -20,7 +20,7 @@ class OsrsranksCommand extends commando.Command
         .setFooter("Last updated")
         .setTimestamp()
         .addField("🗨️ __OSRS Clan__ 🗨️", "This is the official ranking system in place for our OSRS faction of the Clan. As we are a social and community clan, the ranks are based on length of time in the Clan. The amount of time you need to be in the Clan for each rank is as follows:")
-        .addField("😊 __Member Ranks__ 😊", "**Smiley/Member:** 5 days of guesting or applying on forums \n \n **Recruit:** 3 weeks of being a member \n \n **Corporal:** 3 months of being a member \n \n **Sergeant:** 6 months of being a member \n \n **Lieutenant:** 1 year of being a member \n \n **Captain:** 2 years of being a member or being an assistant admin \n \n **General:** Reserved for admin and leadership")
+        .addField("😊 __Member Ranks__ 😊", "**Recruit:** 3 weeks of being a member \n \n **Corporal:** 3 months of being a member \n \n **Sergeant:** 6 months of being a member \n \n **Lieutenant:** 1 year of being a member \n \n **Captain:** 2 years of being a member or being an assistant admin \n \n **General:** Reserved for admin and leadership")
         .addField(" \u200b ", "**:earth_americas: Clan World is 523 :earth_africa:**")
         message.channel.sendEmbed(myInfo);
     }
