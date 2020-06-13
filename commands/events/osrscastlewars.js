e5935d629bb1183d70fae41fd47310ef03ff722f
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-06-13 13:13:53 -0500

    Add files via upload

---
+++ commands/events/osrscastlewars.js
@@ -0,0 +1,53 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js');
+
+class OsrscastlewarsCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'osrscastlewars',
+            group: 'events',
+            memberName: 'osrscastlewars',
+            description: 'Castle wars event details',
+            examples: ['./castlewars "28 September" "20:00" "Describe where to meet and a basic premise here"'],
+            args: [
+                {
+                    key: 'date',
+                    prompt: 'What date is the capping event? (e.g. 28 September)',
+                    type: 'string'
+                },
+                {
+                    key:'time',
+                    prompt: 'What time is the event at? (In game time)',
+                    type: 'string'
+                },
+                {
+                    key: 'world',
+                    prompt: 'What world is it on?',
+                    type: 'string'
+                },
+                {
+                    key: 'description',
+                    prompt: 'Describe the event',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    async run(message, args)
+    {
+        message.delete();
+        var myInfo = new discord.RichEmbed()
+        .setTitle("🏰 __**Castle Wars Event**__ 🏰")
+        .setColor(0x4F2095)
+        .setFooter("Please remember that this is completely for fun! We will have a friendly game of Capture the Flag in which two teams try to bring the opponent's flag back to their base.", "https://oldschool.runescape.wiki/images/5/51/Castle_Wars_logo.png?de0ce")
+        .setThumbnail("https://oldschool.runescape.wiki/images/5/51/Castle_Wars_logo.png?de0ce")
+        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** " + args.world + "\n**Host:** " + message.author)
+        .addField("\u200b", args.description, true)
+      message.channel.send(myInfo);
+    }
+}
+
+module.exports = OsrscastlewarsCommand;
