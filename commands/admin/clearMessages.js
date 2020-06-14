746abefe38b94bf7cb5c4e7c0656b57f1c8418ac
kalmasri123 <kalmasri@me.com>
2020-06-14 19:54:24 -0400

    Update clearMessages.js

--- commands/admin/clearMessages.js
+++ commands/admin/clearMessages.js
@@ -22,7 +22,7 @@ channels.filter(c=>c.type=="text").forEach(async c=>{
     if(messages.size > 0){
         await c.bulkDelete(messages.filter(message=>(Date.now()/(1000*60*60*24)) - (message.createdAt.getTime()/(1000*60*60*24)) < 14))
     }
-    messages.filter(message=>(Date.now()/(1000*60*60*24)) - (message.createdAt.getTime()/(1000*60*60*24)) >= 14)).forEach(message=>{
+    messages.filter(message=>(Date.now()/(1000*60*60*24)) - (message.createdAt.getTime()/(1000*60*60*24)) >= 14).forEach(message=>{
     if(!message.deleted){
       message.delete()
     }
