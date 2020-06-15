054343a7dd84b27e32ed382746dd782a579912ad
kalmasri123 <kalmasri@me.com>
2020-06-14 23:06:39 -0400

    Update clearMessages.js

--- commands/admin/clearMessages.js
+++ commands/admin/clearMessages.js
@@ -26,6 +26,7 @@ class ClearMessagesCommand extends commando.Command
   const messages = (await c.fetchMessages()).filter(message=>{
 //     console.log((Date.now()/(1000*60*60*24)) - (message.createdAt.getTime()/(1000*60*60*24)), !!message.member,message.author.id,id)
 //     console.log(!!message.member)
+      console.log(!message.member,message.author.id,id)
     return !message.member && message.author.id == id
   });
     console.log(messages.size,"SIZE")
