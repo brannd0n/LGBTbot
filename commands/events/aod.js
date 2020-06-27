87cf0ca6b68366de3c104b84c08252bad0570c7f
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-06-26 19:00:39 -0500

    Update aod.js

--- commands/events/aod.js
+++ commands/events/aod.js
@@ -42,6 +42,7 @@ class AodCommand extends commando.Command
         .addField("\u200b", "[Hard mode strategies for Angel of Death](https://runescape.wiki/w/Nex:_Angel_of_Death/Strategies)")
         .addField("\u200b", "**Requirements:**\nFreedom \nAtleast Tier 80 Weapons and Armour \nAntipoision ++ \nMagic or Range Style \nOverloads \nBeast of Burden \nAtleast 71 Prayer", true)
         .addField("\u200b", "**Recommended:**\nVenomblood Perk \nDouble Surge/Escape \nTier 95 Prayers \nVulnerability \nWeapon Poison++ \nManiacal/Reckless Aura \nSign of Life", true)
+        .addBlankField()
         .addField("\u200b", args.description.replace(/[“”‘’"']/g,''), true)
         message.channel.sendEmbed(myInfo);
     }
