c3cddbed4c2400a513e9ed317eea5814df10bc48
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-07-04 10:36:26 -0500

    Update miscrooms.js

--- commands/room/miscrooms.js
+++ commands/room/miscrooms.js
@@ -18,7 +18,7 @@ class MiscRoomCommand extends commando.Command
         var myInfo = new discord.RichEmbed()
         .setTitle("Miscellaneous Rooms")
         .setColor(0x22FF00)
-        .setDescription("<#298258032083861504> - Post pictures of memes or anything random really. \n \n <#241345222984990721> - Nudity is NOT allowed. \n \n <#322392600030085121> - Show us your lovely faces \n \n <#362370714688421888> - Share your food, pretty views, where you have been, anything you are up to irl basically \n \n <#353281345725923328> - Pictures of fur babies in here please \n \n <#331534959657615410> - Share ALL of your music links in here please, no other rooms.")
+        .setDescription("<#717797674220257291> - This room is to discuss all types of diversity including LGBTQ+, BAME/POC representation, Women in Games, Disability & Accessibility, Mental Health & Neurodivergence, Class, Religion, Other. ANYONE who is unable to keep a civil conversation, or begins attacking other members in this room will have the ability to use this channel revoked. \n \n <#298258032083861504> - Post pictures of memes or anything random. \n \n <#411475810592227328> - Use this room for all things runefest \n \n  <#241345222984990721> - Nudity is NOT allowed. \n \n <#322392600030085121> - Show us your lovely faces \n \n <#362370714688421888> - Share your food, hobbies, ect all in here! \n \n <#353281345725923328> - Pictures of fur babies in here please \n \n <#331534959657615410> - Share ALL of your music links in here please, no other rooms.")
         .setFooter("Last updated")
         .setTimestamp();
         message.channel.sendEmbed(myInfo);
