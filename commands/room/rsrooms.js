57e8e1046894fd09cb362db9b9738ceae206c1d0
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-07-04 10:30:48 -0500

    Update rsrooms.js

--- commands/room/rsrooms.js
+++ commands/room/rsrooms.js
@@ -18,7 +18,7 @@ class RsRoomCommand extends commando.Command
         var myInfo = new discord.RichEmbed()
         .setTitle("Runescape Rooms")
         .setColor(0xFBD700)
-        .setDescription("<#634852500775632897> - Please keep the discussions in this room to events related only any other conversations will be removed. \n \n <#266020755760873473> - Please post any and all announcements you have, be they runescape related, birthdays, important LGBT+ news, it all goes in here. \n \n <#330826748822421504> - Share your fashionscape with the clan! \n \n <#266020206973943818> - Please use this room for all runeinfo bot commands \n \n <#408004706720546817> - Show us your drops, clue rewards, skill achievements, goals you have set and how close you are to achieving them. \n \n <#411475810592227328> - Use this room for all things runefest \n \n <#385483677460856842> - Use this room for helpful advice on bosses, clues, quests, etc.")
+        .setDescription("<#715941405981147156> - Use this room to talk about anything RS3 related. \n \n <#634852500775632897> - Please keep the discussions in this room to events related only any other conversations will be removed. \n \n <#266020755760873473> - Please post any and all announcements you have, be they runescape related, birthdays, important LGBT+ news, it all goes in here. \n \n <#330826748822421504> - Share your fashionscape with the clan! \n \n <#408004706720546817> - Post all your boss drops, clue rewards, skill achievements, quests completed and goals achieved here! \n \n <#385483677460856842> - Use this room for helpful advice on bosses, clues, quests, etc. \n \n <#266020206973943818> - Please use this room for all runeinfo bot commands.")
         .setFooter("Last updated")
         .setTimestamp();
         message.channel.sendEmbed(myInfo);
@@ -26,3 +26,4 @@ class RsRoomCommand extends commando.Command
 }
 
 module.exports = RsRoomCommand;
+
