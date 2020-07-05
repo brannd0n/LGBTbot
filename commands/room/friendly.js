0ed35162a227eeb9a08f44e7c27b72b490adb415
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-07-04 19:10:56 -0500

    Add files via upload

---
+++ commands/room/friendly.js
@@ -0,0 +1,28 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js');
+
+class FriendlyCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'friendly',
+            group: 'room',
+            memberName: 'friendly',
+            description: 'Friendly bot info.'
+        });
+    }
+
+    async run(message, args)
+    {
+        var myInfo = new discord.RichEmbed()
+        .setTitle("🤖 __**Friendly BOT COMMANDS**__ 🤖")
+        .setColor(0xFD00FF)
+        .setDescription(" `-setrsn` username -- Set rsn to show your stats. `-alog` -- Displays adventure log. \n \n `-stats` -- Displays RS3 stats. \n \n `-07stats` -- Displays OSRS stats.\n \n `-perks` -- Displays current perks available in `-perk` command. \n \n `-perk` -- Displays fortune perk combinations. \n \n `-pylon` -- Displays pylon percentage. \n \n `-split` -- Displays GP split for the number of people in the group.")
+        .setFooter("Last updated")
+        .setTimestamp();
+        message.channel.sendEmbed(myInfo);
+    }
+}
+
+module.exports = FriendlyCommand;
