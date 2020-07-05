f17e6c2ec82b90d4909349bf41ac2904fcc7d48c
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-07-05 13:19:17 -0500

    Update friendly.js

--- commands/room/friendly.js
+++ commands/room/friendly.js
@@ -18,7 +18,7 @@ class FriendlyCommand extends commando.Command
         var myInfo = new discord.RichEmbed()
         .setTitle("🤖 __**Friendly BOT COMMANDS**__ 🤖")
         .setColor(0xFD00FF)
-        .setDescription(" `-setrsn` username -- Set rsn to show your stats. `-alog` -- Displays adventure log. \n \n `-stats` -- Displays RS3 stats. \n \n `-07stats` -- Displays OSRS stats.\n \n `-perks` -- Displays current perks available in `-perk` command. \n \n `-perk` -- Displays fortune perk combinations. \n \n `-pylon` -- Displays pylon percentage. \n \n `-split` -- Displays GP split for the number of people in the group.")
+        .setDescription(" `-setrsn` username -- Set rsn to show your stats. `-alog` -- Displays adventure log. \n \n `-stats` -- Displays RS3 stats. \n \n `-07stats` -- Displays OSRS stats.\n \n `-perks` -- Displays current perks available in `-perk` command. \n \n `-perk` -- Displays the combination for the perk your looking for. For example ‘-perk honed’ for honed combination. \n \n `-pylon` -- Displays pylon percentage. \n \n `-split` -- Displays GP split for the number of people in the group.")
         .setFooter("Last updated")
         .setTimestamp();
         message.channel.sendEmbed(myInfo);
