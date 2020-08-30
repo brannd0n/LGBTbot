f810619456d22d78e795a6bcf05e0e9e47366d90
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-08-30 01:24:50 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 const Commando = require('discord.js-commando');
 const http = require('http');
+const { nextTick } = require('process');
 const TOKEN = process.env.token || JSON.parse(fs.readFileSync('./config.json')).token;
+const admin = require('firebase-admin');
+const firebase = require('./FirebaseServiceAccount.json');
+// Initialize DB
+admin.initializeApp({ credential: admin.credential.cert(firebase) });
+const db = admin.firestore();
 
 const bot = new Commando.Client({
     commandPrefix: './'
@@ -16,6 +22,133 @@ bot.registry
     .registerDefaults()
     .registerCommandsIn(__dirname + '/commands')
     ;
+
+bot.on('guildCreate', async guild => {
+    try {
+        await db.collection('guilds').doc(guild.id).set({
+            messages: {}
+        });
+        console.log(`${guild.name} added`);
+    }
+    catch (error) {
+        console.error(error);
+    }
+});
+
+bot.on('guildDelete', async guild => {
+    try {
+        await db.collection('guilds').doc(guild.id).delete();
+        console.log(`${guild.name} deleted`);
+    }
+    catch (error) {
+        console.error(error);
+    }
+});
+
+
+bot.on('raw', async event => {
+    const eventName = event.t;
+    if (eventName === 'MESSAGE_REACTION_ADD') {
+        const snapshot = await db.collection('guilds').doc(event.d.guild_id).get();
+        const messages = snapshot.data().messages;
+        const message_id_array = Object.keys(messages).map(function (key, index) {
+            return key;
+        });
+        if (message_id_array.includes(event.d.message_id)) {
+            const reactionChannel = bot.channels.get(event.d.channel_id);
+            if (reactionChannel.messages.has(event.d.message_id)) {
+                return;
+            }
+            else {
+                reactionChannel.fetchMessage(event.d.message_id)
+                    .then(message => {
+                        let messageReaction = message.reactions.get(`${event.d.emoji.name}:${event.d.emoji.id}`);
+                        if (!messageReaction)
+                            messageReaction = message.reactions.get(`${event.d.emoji.name}`);
+
+                        const user = bot.users.get(event.d.user_id);
+                        bot.emit('MessageReactionAdd', messageReaction, user);
+                    })
+                    .catch(error => console.log(error));
+            }
+        }
+    }
+    else if (eventName === 'MESSAGE_REACTION_REMOVE') {
+        const snapshot = await db.collection('guilds').doc(event.d.guild_id).get();
+        const messages = snapshot.data().messages;
+        const message_id_array = Object.keys(messages).map(function (key, index) {
+            return key;
+        });
+        if (message_id_array.includes(event.d.message_id)) {
+            const reactionChannel = bot.channels.get(event.d.channel_id);
+            if (reactionChannel.messages.has(event.d.message_id)) {
+                return;
+            }
+            else {
+                reactionChannel.fetchMessage(event.d.message_id)
+                    .then(message => {
+                        let messageReaction = message.reactions.get(`${event.d.emoji.name}:${event.d.emoji.id}`);
+                        if (!messageReaction)
+                            messageReaction = message.reactions.get(`${event.d.emoji.name}`);
+
+                        const user = bot.users.get(event.d.user_id);
+                        bot.emit('MessageReactionRemove', messageReaction, user);
+                    })
+                    .catch(error => console.log(error));
+            }
+        }
+    }
+})
+
+bot.on('messageReactionAdd', async (messageReaction, user) => {
+    const guild_id = messageReaction.message.channel.guild.id;
+    const message_id = messageReaction.message.id;
+    const snapshot = await db.collection('guilds').doc(guild_id).get();
+    const messages = snapshot.data().messages;
+    const required_message_info = messages[message_id];
+
+    const emojiName = messageReaction.emoji.name;
+    const emojiID = messageReaction.emoji.id;
+    const emoji = emojiID ? `<:${emojiName}:${emojiID}>` : emojiName;
+
+    console.log(emoji);
+    
+    const role_id = required_message_info.find(el => el.emoji === emoji).role_id;
+    const role = messageReaction.message.guild.roles.find(role => role.id === role_id);
+    console.log(role_id, role);
+    if (role) {
+        const member = messageReaction.message.guild.members.find(member => member.id === user.id);
+        if (member) {
+            member.addRole(role.id);
+            console.log('success');
+        }
+    }
+});
+
+bot.on('messageReactionRemove', async (messageReaction, user) => {
+    const guild_id = messageReaction.message.channel.guild.id;
+    const message_id = messageReaction.message.id;
+    const snapshot = await db.collection('guilds').doc(guild_id).get();
+    const messages = snapshot.data().messages;
+    const required_message_info = messages[message_id];
+
+    const emojiName = messageReaction.emoji.name;
+    const emojiID = messageReaction.emoji.id;
+    const emoji = emojiID ? `<:${emojiName}:${emojiID}>` : emojiName;
+
+    console.log(emoji);
+    
+    const role_id = required_message_info.find(el => el.emoji === emoji).role_id;
+    const role = messageReaction.message.guild.roles.find(role => role.id === role_id);
+    console.log(role_id, role);
+    if (role) {
+        const member = messageReaction.message.guild.members.find(member => member.id === user.id);
+        if (member) {
+            member.removeRole(role.id);
+            console.log('success');
+        }
+    }
+})
 
 bot.on('ready', function () {
     console.log("Ready");
