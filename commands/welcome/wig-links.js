8759ffa0ed4aaecbc2b6f7cba36b34aa5ae71907
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-07 19:56:03 -0500

    Update wig-links.js

--- commands/welcome/wig-links.js
+++ commands/welcome/wig-links.js
@@ -20,7 +20,7 @@ class WigLinksCommand extends commando.Command
         .addField("**Women in Games links**", "🐦 Twitter [@wigj](https://twitter.com/wigj)\n\n" +
         "🇫 [Facebook Group @womeningameswigj](https://www.facebook.com/groups/womeningameswigj/)\n\n" +
         ":office: [LinkdIN Company Page](https://www.linkedin.com/company/wigj/)\n\n" +
-        ":speech_left: [LinkdIN Group](https://www.linkedin.com/groups/2739553/))
+        ":speech_left: [LinkdIN Group](https://www.linkedin.com/groups/2739553/)")
         .setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/180/rainbow_1f308.png")
         .setFooter("Last updated")
         .setTimestamp();
