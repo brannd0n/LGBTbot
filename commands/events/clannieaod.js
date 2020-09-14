432e0fff1c56038555e6da2c26ea66b349ee44d9
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 14:30:54 -0500

    Update clannieaod.js

--- commands/events/clannieaod.js
+++ commands/events/clannieaod.js
@@ -36,17 +36,26 @@ class ClannieAodCommand extends commando.Command
     }
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            host:  filtered[2] || '',
+            description: filtered[3] || '',
+        };
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("⚔️ __**Angel of Death Event**__ ⚔️")
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! If you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/2/2b/Nex_%28Angel_of_Death%29.png?00050")
         .setThumbnail("https://runescape.wiki/images/2/2b/Nex_%28Angel_of_Death%29.png?00050")
-        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + args.host)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + new_args.host)
         .addField("\u200b", "[Hard mode strategies for Angel of Death](https://runescape.wiki/w/Nex:_Angel_of_Death/Strategies)")
         .addField("\u200b", "**Requirements:**\nFreedom \nAtleast Tier 80 Weapons and Armour \nAntipoision ++ \nMagic or Range Style \nOverloads \nBeast of Burden \nAtleast 71 Prayer", true)
         .addField("\u200b", "**Recommended:**\nVenomblood Perk \nDouble Surge/Escape \nTier 95 Prayers \nVulnerability \nWeapon Poison++ \nManiacal/Reckless Aura \nSign of Life", true)
-        .addField("\u200b", args.description, true);
+        .addField("\u200b", new_args.description, true);
         message.channel.sendEmbed(myInfo);
     }
 }
