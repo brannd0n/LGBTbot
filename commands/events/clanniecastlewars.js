d2bcb25faa3e512c37289003d6f3dc8d4d357704
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 14:42:20 -0500

    Update clanniecastlewars.js

--- commands/events/clanniecastlewars.js
+++ commands/events/clanniecastlewars.js
@@ -38,14 +38,24 @@ class ClannieCastlewarsCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            host:  filtered[2] || '',
+            description: filtered[3] || '',
+        };
+        
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("🏰 __**Castle Wars Event**__ 🏰")
         .setColor(0x4F2095)
         .setFooter("Please remember that this is completely for fun! We will have a friendly game of Capture the Flag in which two teams try to bring the opponent's flag back to their base.", "https://vignette.wikia.nocookie.net/runescape2/images/4/49/Castle_Wars_icon.jpg/revision/latest?cb=20080728140445")
         .setThumbnail("https://vignette.wikia.nocookie.net/runescape2/images/4/49/Castle_Wars_icon.jpg/revision/latest?cb=20080728140445")
-        .addField("\u200b","**Date:** " + args.date + "\n**Time:** " + args.time + "\n**Host: **" + args.host +"\n**World:** W23")
-        .addField("\u200b", args.description, true)
+        .addField("\u200b","**Date:** " + new_args.date + "\n**Time:** " + new_args.time + "\n**Host: **" + new_args.host +"\n**World:** W23")
+        .addField("\u200b", new_args.description, true)
       message.channel.send(myInfo);
     }
 }
