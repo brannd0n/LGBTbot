d842678e429d62e917345be60bbf44378fef73dd
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 13:51:55 -0500

    Update barbassault.js

--- commands/events/barbassault.js
+++ commands/events/barbassault.js
@@ -33,14 +33,23 @@ class BarbAssaultCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            description:  filtered[2] || '',
+        };
+        
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("<:yellow_egg:677335122034294790>  __**Barbarian Assault**__ <:yellow_egg:677335122034294790>")
         .setColor(0x4F2095)
         .setFooter("Please remember that this is completely for fun! This is a cooperative, team-based safe combat minigame located at the Barbarian Outpost. A team of five players must work together in order to defeat ten waves of Penance monsters and, finally, their leader, the Penance Queen.", "https://runescape.wiki/images/6/69/Barbarian_Assault_icon.jpg?7ae4a")
         .setThumbnail("https://runescape.wiki/images/6/69/Barbarian_Assault_icon.jpg?7ae4a")
-        .addField("\u200b","📅 **Date:** " + args.date.replace(/[“”‘’"']/g,'') + "\n🕘 **Time:** " + args.time.replace(/[“”‘’"']/g,'') + " game-time\n🌍 **World:** 23" + "\n**Host:** " + message.author)
-        .addField("\u200b", args.description.replace(/[“”‘’"']/g,''), true)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23" + "\n**Host:** " + message.author)
+        .addField("\u200b", new_args.description, true)
         message.channel.send(myInfo);
     }
 }
