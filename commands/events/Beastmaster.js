b9b8489e94b03eaef42ba002dbf1b511d3af4319
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 12:40:56 -0500

    Update Beastmaster.js

--- commands/events/Beastmaster.js
+++ commands/events/Beastmaster.js
@@ -30,8 +30,17 @@ class BeastmasterCommand extends commando.Command
         });
     }
 
-    async run(message, args)
+async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            description:  filtered[2] || '',
+           };
+
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("⚔️ __**Beastmaster Durzag**__ ⚔️")
