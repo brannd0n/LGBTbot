0a6c08191703b68bbb2ff07020ebfc78fbeeeaed
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 14:37:37 -0500

    Update clanniecabbage.js

--- commands/events/clanniecabbage.js
+++ commands/events/clanniecabbage.js
@@ -38,14 +38,23 @@ class ClannieCabbageCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            host:  filtered[2] || '',
+            description: filtered[3] || '',
+        };
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle(":gorilla:  __**Cabbage Facepunch Bonanza Event**__ :gorilla: ")
         .setColor(0x4F2095)
         .setFooter("Please remember that this is completely for fun! This minigame involves a maze where players have to survive as long as possible, running between two safe zones. Any player who fails is turned into a gorilla.", "https://vignette.wikia.nocookie.net/runescape2/images/8/82/Cabbage_Facepunch_Bonanza_icon.png/revision/latest?cb=20170322194600")
         .setThumbnail("https://vignette.wikia.nocookie.net/runescape2/images/8/82/Cabbage_Facepunch_Bonanza_icon.png/revision/latest?cb=20170322194600")
-        .addField("\u200b","**Date:** " + args.date + "\n**Time:** " + args.time + "\n**Host: **" + args.host +"\n**World:** W23")
-        .addField("\u200b", args.description, true)
+        .addField("\u200b","**Date:** " + new_args.date + "\n**Time:** " + new_args.time + "\n**Host: **" + new_args.host +"\n**World:** W23")
+        .addField("\u200b", new_args.description, true)
         message.channel.send(myInfo);
     }
 }
