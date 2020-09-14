91bfc4cb1f8bbfbe6e88eff30f1478c17fb9adbf
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 15:57:56 -0500

    Update clanniecorp.js

--- commands/events/clanniecorp.js
+++ commands/events/clanniecorp.js
@@ -37,17 +37,27 @@ class ClannieCorpCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            host:  filtered[2] || '',
+            description: filtered[3] || '',
+        };
+        
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("⚔️ __**Corporeal Beast MASS**__ ⚔️")
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! Make sure you bring any type of spear! All other weapons will only do half damage. Just hit Corp and avoid the Dark Energy Core, do **NOT** bring a BoB as Corp will eat it. A games necklace can be used to avoid the wilderness and teleport to the beast's cave. That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/thumb/5/5c/Corporeal_Beast.png/250px-Corporeal_Beast.png?36acd")
         .setThumbnail("https://runescape.wiki/images/thumb/5/5c/Corporeal_Beast.png/250px-Corporeal_Beast.png?36acd")
-        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + args.host)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + new_args.host)
         .addField("\u200b", "[Strategies for Corporeal Beast (wiki)](https://runescape.wiki/w/Corporeal_Beast/Strategies)")
         .addField("\u200b", "**Requirements:**\nSummer's End quest completed \nAny spear", true)
         .addField("\u200b", "**Recommended:**\nBandos armour or higher ", true)
-        .addField("\u200b", args.description, true);
+        .addField("\u200b", new_args.description, true);
         message.channel.sendEmbed(myInfo);
     }
 }
