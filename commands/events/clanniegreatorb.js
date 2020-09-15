9df1551cebed194ec117193eb0632008f96cfa3c
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 19:49:32 -0500

    Update clanniegreatorb.js

--- commands/events/clanniegreatorb.js
+++ commands/events/clanniegreatorb.js
@@ -33,13 +33,21 @@ class ClannieGreatorbCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            host:  filtered[2] || '',
+        };
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle(":sparkles: __**Great Orb Project Event**__ :sparkles:")
         .setColor(0x4F2095)
         .setFooter("Please remember that this is completely for fun! We will have two friendly teams compete to collect orbs while preventing the other team from collecting theirs. Winners of each round gain Runecrafting rewards.", "https://vignette.wikia.nocookie.net/runescape2/images/d/da/Mg_orbproject.jpg/revision/latest?cb=20080812130710")
-            .setThumbnail("https://runescape.wiki/images/thumb/9/9b/Master_runecrafter_hat_detail.png/261px-Master_runecrafter_hat_detail.png")
-        .addField("\u200b","**Date:** " + args.date + "\n**Time:** " + args.time + "\n**Host: **" + args.host +"\n**World:** W23")
+        .setThumbnail("https://runescape.wiki/images/thumb/9/9b/Master_runecrafter_hat_detail.png/261px-Master_runecrafter_hat_detail.png")
+        .addField("\u200b","**Date:** " + new_args.date + "\n**Time:** " + new_args.time + "\n**Host: **" + new_args.host +"\n**World:** W23")
         .addField("\u200b", "**Requirements:** \n50 Runecrafting  ", true)
         message.channel.send(myInfo);
     }
