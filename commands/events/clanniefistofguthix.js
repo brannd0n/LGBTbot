1dd9ae4a6ce10a224ecf1f6bc9d8e49eb8178200
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 19:35:03 -0500

    Update clanniefistofguthix.js

--- commands/events/clanniefistofguthix.js
+++ commands/events/clanniefistofguthix.js
@@ -33,13 +33,21 @@ class ClannieFistofguthixCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            host:  filtered[2] || '',
+        };
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle(":green_heart:__**Fist of Guthix Event**__:green_heart: ")
         .setColor(0x4F2095)
         .setFooter("Please remember that this is completely for fun! This minigame is a tactical one-on-one game with two rounds, in which one player collects charges with a magical stone while the other is trying to hunt them down. In the second round, the players switch sides", "https://vignette.wikia.nocookie.net/runescape2/images/1/17/Fist_of_Guthix_icon.jpg/revision/latest?cb=20080728141256")
         .setThumbnail("https://runescape.wiki/images/f/fd/Battle_hood_detail.png")
-        .addField("\u200b","**Date:** " + args.date + "\n**Time:** " + args.time + "\n**Host: **" + args.host +"\n**World:** W23")
+        .addField("\u200b","**Date:** " + new_args.date + "\n**Time:** " + new_args.time + "\n**Host: **" + new_args.host +"\n**World:** W23")
         message.channel.send(myInfo);
     }
 }
