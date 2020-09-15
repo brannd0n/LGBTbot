8758b74ba936b44218c8d0f03f09d395f79ae8d2
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 22:30:31 -0500

    Update clanniehardkree.js

--- commands/events/clanniehardkree.js
+++ commands/events/clanniehardkree.js
@@ -37,17 +37,29 @@ class ClannieHardkreeCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            event: filtered[0] || '',
+            date:  filtered[1] || '',
+            time:  filtered[2] || '',
+            world: filtered[3] || '',
+            where: filtered[4] || '',
+            description: filtered[5] || '\u200B',
+        };
+        
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("⚔️ __**HARDMODE Kree MASS**__ ⚔️")
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! Just hit Kree and avoid the specials. That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/f/fd/Kree%27arra.png")
         .setThumbnail("https://runescape.wiki/images/f/fd/Kree%27arra.png")
-        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + args.host)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + new_args.host)
         .addField("\u200b", "[Hard mode strategies for Kree'arra](https://runescape.wiki/w/Kree%27arra/Strategies/Hard_mode)")
         .addField("\u200b", "**Requirements:**\nTroll Stronghold \n70 Ranged", true)
         .addField("\u200b", "**Recommended:**\nTier 70 Armour or higher \n Overloads \n Soulsplit", true)
-        .addField("\u200b", args.description, true)
+        .addField("\u200b", new_args.description, true)
         message.channel.sendEmbed(myInfo);
     }
 }
