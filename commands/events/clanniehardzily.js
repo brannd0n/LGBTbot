f2dc1ea8e28afbbba433ed53873f0194e03a0495
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 22:40:55 -0500

    Update clanniehardzily.js

--- commands/events/clanniehardzily.js
+++ commands/events/clanniehardzily.js
@@ -37,17 +37,27 @@ class ClannieHardzilyCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            host:  filtered[2] || '',
+            description: filtered[3] || '',
+        };
+        
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("⚔️ __**HARDMODE Zilyana MASS**__ ⚔️")
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! Just hit Zily and avoid her attacks. She has 2 phases in hardmode and must be finished off with an Ultimate ability that deals damage! That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/f/fb/Commander_Zilyana.png?4fd88")
         .setThumbnail("https://runescape.wiki/images/f/fb/Commander_Zilyana.png?4fd88")
-        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + args.host)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + new_args.host)
         .addField("\u200b", "[Hard mode strategies for Commander Zilyana](https://runescape.wiki/w/Commander_Zilyana/Strategies/Hard_mode)")
         .addField("\u200b", "**Requirements:**\nTroll Stronghold \n70 Agility", true)
         .addField("\u200b", "**Recommended:**\nTier 70 Armour or higher \n Overloads \n Soulsplit", true)
-        .addField("\u200b", args.description, true)
+        .addField("\u200b", new_args.description, true)
         message.channel.sendEmbed(myInfo);
     }
 }
