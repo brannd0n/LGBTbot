d74a96a7e96b5442f3bd912ed3320bf52077534d
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 19:24:17 -0500

    Update clanniecapping.js

--- commands/events/clanniecapping.js
+++ commands/events/clanniecapping.js
@@ -38,14 +38,23 @@ class ClannieCappingCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            theme:  filtered[1] || '',
+            host:  filtered[2] || '',
+            description: filtered[3] || '',
+        };
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("🏰 __**CITADEL CAPPING**__ 🏰")
         .setColor(0x00AE86)
         .setFooter("When skilling with fellow clan members at the same time in the Citadel, the resource gain has been slightly increased, the more members you gather resources with at once, the better the gain!", "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/facebook/138/european-castle_1f3f0.png")
         .setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/129/european-castle_1f3f0.png")
-        .addField("\u200b","**Date:** Friday " + args.date + "\n**Time:** 19:30 game-time\n**Host: **" + args.host +" \n**Theme: **" + args.theme)
-        .addField("\u200b", args.description, true);
+        .addField("\u200b","**Date:** Friday " + new_args.date + "\n**Time:** 19:30 game-time\n**Host: **" + new_args.host +" \n**Theme: **" + new_args.theme)
+        .addField("\u200b", new_args.description, true);
         message.channel.sendEmbed(myInfo);
     }
 }
