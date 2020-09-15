80ed6e279228599f5d7b6528091585f2d703aebe
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-15 12:37:26 -0500

    Update clannietroublebrewing.js

--- commands/events/clannietroublebrewing.js
+++ commands/events/clannietroublebrewing.js
@@ -33,13 +33,22 @@ class ClannieTroublebrewingCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            host:  filtered[2] || '',
+        };
+        
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("🏰 __**Trouble Brewing Event**__ 🏰")
         .setColor(0x4F2095)
         .setFooter("Please remember that this is completely for fun! We will have Two friendly teams try to brew the most bottles of 'rum'. Experience in a variety of skills can be earned for performing the many tasks needed in the process of 'rum' production.", "https://vignette.wikia.nocookie.net/runescape2/images/b/be/Mg_troublebrewing.jpg/revision/latest?cb=20080728164336")
         .setThumbnail("https://runescape.wiki/images/b/be/Mg_troublebrewing.jpg")
-        .addField("\u200b","**Date:** " + args.date + "\n**Time:** " + args.time + "\n**Host: **" + args.host +"\n**World:** W23")
+        .addField("\u200b","**Date:** " + new_args.date + "\n**Time:** " + new_args.time + "\n**Host: **" + new_args.host +"\n**World:** W23")
         .addField("\u200b", "**Requirements:**\n40 Cooking and completed Cabin Fever Quest", true)
         message.channel.send(myInfo);
     }
