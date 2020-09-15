12f1281fbddb8534034a454ea91388c7a09e550e
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-15 12:21:40 -0500

    Update clannieshades.js

--- commands/events/clannieshades.js
+++ commands/events/clannieshades.js
@@ -38,16 +38,26 @@ class ClannieShadesCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            host:  filtered[2] || '',
+            description: filtered[3] || '',
+        };
+        
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle(":ghost:__**Shades of Mort'ton Event**__:ghost:")
         .setColor(0x4F2095)
         .setFooter("Please remember that this is completely for fun! We will rebuild the Flamtaer temple under siege from the Loar Shades, and create the ingredients to make forms of pyre logs which are used to cremate shade remains. We will then enter the Shade Catacombs under the town, hunting for the treasure chests which are unlocked by the various keys received by burning the shades. You may also enter the catacombs simply to hunt the more powerful shades that lurk within.", "https://runescape.wiki/images/e/e8/Mg_shadesofmortton.jpg?432ce")
         .setThumbnail("https://runescape.wiki/images/e/e8/Mg_shadesofmortton.jpg?432ce")
-        .addField("\u200b","**Date:** " + args.date + "\n**Time:** " + args.time + "\n**Host: **" + args.host +"\n**World:** W23")
+        .addField("\u200b","**Date:** " + new_args.date + "\n**Time:** " + new_args.time + "\n**Host: **" + new_args.host +"\n**World:** W23")
         .addField("\u200b", "**Requirements:**\nShades of Mort'ton quest", true)
         .addField("\u200b", "**Recommended:**\nTimber Beam or Plank \nLimestone Brick \nSwamp Paste \nPyre Logs (any type)", true)
-        .addField("\u200b", args.description, true);
+        .addField("\u200b", new_args.description, true);
         message.channel.send(myInfo);
     }
 }
