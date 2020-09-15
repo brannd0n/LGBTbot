6c2d944c371032ae88f0432ae1e7c3e6cc7d39ac
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 19:37:39 -0500

    Update clanniegraar.js

--- commands/events/clanniegraar.js
+++ commands/events/clanniegraar.js
@@ -37,17 +37,27 @@ class ClannieGraarCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            host:  filtered[2] || '',
+            description: filtered[3] || '',
+        };
+        
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("⚔️ __**Graardor MASS**__ ⚔️")
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! Just hit Graardor and avoid the specials. That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/thumb/b/b8/General_Graardor.png/375px-General_Graardor.png?3b673")
         .setThumbnail("https://runescape.wiki/images/thumb/b/b8/General_Graardor.png/375px-General_Graardor.png?3b673")
-        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + args.host)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + new_args.host)
         .addField("\u200b", "[Normal Mode strategies for General Graardor](https://runescape.fandom.com/wiki/General_Graardor/Strategies)")
         .addField("\u200b", "**Requirements:**\nTroll Stronghold \n70 Strength", true)
         .addField("\u200b", "**Recommended:**\nTier 70 Armour or higher \n Overloads \n Super Prayer Renewals", true)
-        .addField("\u200b", args.description, true)
+        .addField("\u200b", new_args.description, true)
         message.channel.sendEmbed(myInfo);
     }
 }
