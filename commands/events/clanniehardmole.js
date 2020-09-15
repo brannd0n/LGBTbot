f66dc16586ecd1e17e0cab44229e5b29fa782629
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-14 22:34:47 -0500

    Update clanniehardmole.js

--- commands/events/clanniehardmole.js
+++ commands/events/clanniehardmole.js
@@ -32,13 +32,22 @@ class ClannieHardMoleCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            host:  filtered[2] || '',
+        };
+        
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("⚔️ __**Hardmode Giant Mole Mass**__ ⚔️")
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! Just follow the mole and hit her. That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/thumb/f/fd/Giant_Mole.png/240px-Giant_Mole.png?0e85e")
         .setThumbnail("https://runescape.wiki/images/thumb/f/fd/Giant_Mole.png/240px-Giant_Mole.png?0e85e")
-        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + args.host)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + new_args.host)
         .addField("\u200b", "[Strategies for Hardmode Giant mole](https://runescape.wiki/w/Giant_mole/Strategies)")
         .addField("\u200b", "**Requirements:**\nNone", true)
         .addField("\u200b", "**Recommended:**\nFood \nPrayer Potions \nFreedom/Anticipation unlocked", true)
