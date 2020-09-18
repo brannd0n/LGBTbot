32df473f10295dbbd5ac739222ee62ec579c32ee
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-18 16:27:46 -0500

    Update ed3.js

--- commands/events/ed3.js
+++ commands/events/ed3.js
@@ -32,16 +32,25 @@ class Ed3Command extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            description:  filtered[2] || '\u200B',
+        };
+        
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("⚔️__**Shadow Reef Event**__⚔️")
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! Please feel free to PM me. All loot will be traded to me and split equally between attendees.", "https://runescape.wiki/images/7/7e/The_Ambassador.png?efb9b")
         .setThumbnail("https://runescape.wiki/images/7/7e/The_Ambassador.png?efb9b")
-        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + message.author)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + message.author)
         .addField("\u200b", "[Strategies for The Shadow Reef](https://runescape.wiki/w/The_Shadow_Reef/Strategies)")
         .addField("\u200b", "**Recommended:**\nTier 80 weapons or higher \nSara brew flasks \nSalve amulet (e) \nEnhanced Excalibur and any mainhand Melee weapon for Bladed Dive", true)
-        .addField("\u200b", args.description, true)
+        .addField("\u200b", new_args.description, true)
         message.channel.sendEmbed(myInfo);
     }
 }
