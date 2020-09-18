de11c0bc6cc845fee829a0dbb8dbf617aee08880
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-18 16:43:30 -0500

    Update flashpowderfactory.js

--- commands/events/flashpowderfactory.js
+++ commands/events/flashpowderfactory.js
@@ -33,16 +33,24 @@ class FlashpowderfactoryCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            description: filtered[2] || '\u200B',
+        };
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle(":gear: __**Flash Powder Factory Event**__ :gear:")
         .setColor(0x4F2095)
         .setFooter("Please remember that this is completely for fun! This is a minigame that uses Agility, Thieving and Herblore, requiring players to collect the reagents for making flash powder and take them to the powder making machines, while traversing obstacles and pickpocketing other players (or being pickpocketed themselves). Reward points from the game can be swapped for experience or parts of the factory outfit. ", "https://vignette.wikia.nocookie.net/runescape2/images/c/c4/Mg_roguesden.jpg/revision/latest?cb=20080728143017")
         .setThumbnail("https://runescape.wiki/images/thumb/8/8f/Factory_top_detail.png/261px-Factory_top_detail.png?152ce")
-        .addField("\u200b","📅 **Date:** " + args.date.replace(/[“”‘’"']/g,'') + "\n🕘 **Time:** " + args.time.replace(/[“”‘’"']/g,'') + " game-time\n🌍 **World:** 23" + "\n**Host:** " + message.author)
+        .addField("\u200b","📅 **Date:** " + new_args.date) + "\n🕘 **Time:** " + new_args.time) + " game-time\n🌍 **World:** 23" + "\n**Host:** " + message.author)
         .addField("\u200b", "**Requirements:**\n75 Theiving & Agility \n50 Herblore  ", true)
         .addBlankField()
-        .addField("\u200b", args.description.replace(/[“”‘’"']/g,''), true)
+        .addField("\u200b", new_args.description), true)
         message.channel.send(myInfo);
     }
 }
