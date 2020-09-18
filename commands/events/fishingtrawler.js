298867bd051903b7ae2d7f55e7a476f3e82c71b5
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-18 16:33:27 -0500

    Update fishingtrawler.js

--- commands/events/fishingtrawler.js
+++ commands/events/fishingtrawler.js
@@ -32,14 +32,22 @@ class FishingtrawlerCommand extends commando.Command
           }
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            description: filtered[2] || '\u200B',
+        };
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle(":fish: __**Fishing Trawler Event**__ :fish:")
         .setColor(0x4F2095)
         .setFooter("Please remember that this is completely for fun! We will work together to keep a trawler afloat while keeping nets fixed in order to catch as many fish as possible.", "https://vignette.wikia.nocookie.net/runescape2/images/1/14/Mg_fishingtrawler.jpg/revision/latest?cb=20080728145136")
         .setThumbnail("https://runescape.wiki/images/thumb/b/b2/Raw_manta_ray_detail.png/237px-Raw_manta_ray_detail.png?668eb")
-        .addField("\u200b","**Date:** " + args.date + "\n**Time:** " + args.time + "\n**Host: **" + message.author +"\n**World:** W23")
-        .addField("\u200b", args.description, true)
+        .addField("\u200b","**Date:** " + new_args.date + "\n**Time:** " + new_args.time + "\n**Host: **" + message.author +"\n**World:** W23")
+        .addField("\u200b", new_args.description, true)
       message.channel.send(myInfo);
     }
 }
