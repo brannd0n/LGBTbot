bd5450dda5a46a7f9f99e632eef82b4791568752
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-18 00:19:20 -0500

    Update dagkings.js

--- commands/events/dagkings.js
+++ commands/events/dagkings.js
@@ -20,6 +20,11 @@ class DagkingsCommand extends commando.Command
                     key: 'time',
                     prompt: 'What time is the event? (e.g. 21:00)',
                     type: 'string'
+                },
+                {
+                    key: 'description',
+                    prompt: 'Describe the event',
+                    type: 'string'
                 }
             ]
         });
@@ -27,16 +32,27 @@ class DagkingsCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            description:  filtered[2] || '',
+        };
+        
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("⚔️ __**DAGANNOTH KINGS MASS**__ ⚔️")
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! Attack the King that's weak to the combat style you bring! That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/thumb/b/b4/Dagannoth_Supreme.png/800px-Dagannoth_Supreme.png?8fbad")
         .setThumbnail("https://runescape.wiki/images/thumb/b/b4/Dagannoth_Supreme.png/800px-Dagannoth_Supreme.png?8fbad")
-        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + message.author)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + message.author)
         .addField("\u200b", "[Strategies for Dagannoth Kings](https://runescape.wiki/w/Dagannoth_Kings/Strategies)")
         .addField("\u200b", "**Requirements:**\nNone", true)
         .addField("\u200b", "**Recommended:**\nDag Kings TeleTab\n90+ Herb for extremes\nAnything above barrows armour", true)
+        .addBlankField()
+        .addField("\u200b",new_args.description, true);
         message.channel.sendEmbed(myInfo);
     }
 }
