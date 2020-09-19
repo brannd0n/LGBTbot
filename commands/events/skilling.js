6f25bd688173a68bed7f483ff9adfcd66957f042
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-18 21:30:22 -0500

    Update skilling.js

--- commands/events/skilling.js
+++ commands/events/skilling.js
@@ -37,13 +37,22 @@ class SkillingCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            location:  filtered[2] || '',
+            description: filtered[3] || '\u200B',
+        };
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("📊 __**Portables and Pulsecores**__ 📊")
         .setColor(0x00AE86)
         .setThumbnail("https://runescape.wiki/images/5/52/Completionist_cape_%28t%29_detail.png?9fa02")
-        .addField("\u200b","**Date:** " + args.date + "\n**Time:** " + args.time + "\n**Host: **" + message.author +"\n**Location: ** " + args.location)
-        .addField("\u200b", args.description, true);
+        .addField("\u200b","**Date:** " + new_args.date + "\n**Time:** " + new_args.time + "\n**Host: **" + message.author +"\n**Location: ** " + new_args.location)
+        .addField("\u200b", new_args.description, true);
         message.channel.sendEmbed(myInfo);
     }
 }
