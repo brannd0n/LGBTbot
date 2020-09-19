7087595739165681c5fe8ced59c78db932c9ac29
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-18 19:16:06 -0500

    Update osrsbank.js

--- commands/events/osrsbank.js
+++ commands/events/osrsbank.js
@@ -33,14 +33,22 @@ class OsrsbankCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            description: filtered[2] || '\u200B',
+        };
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("<:skills:458333529651281921> __**Bank Standing**__ <:skills:458333529651281921>")
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun!", "https://oldschool.runescape.wiki/images/c/cc/Bank_chest.png?c38c7")
         .setThumbnail("https://oldschool.runescape.wiki/images/c/cc/Bank_chest.png?c38c7")
-        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 523\n**Host:** " + message.author)
-        .addField("\u200b", args.description, true)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 523\n**Host:** " + message.author)
+        .addField("\u200b", new_args.description, true)
         message.channel.sendEmbed(myInfo);
     }
 }
