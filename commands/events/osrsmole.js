e3849892eaf872cc715bf34cd0779552a09ff28f
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-18 20:43:09 -0500

    Update osrsmole.js

--- commands/events/osrsmole.js
+++ commands/events/osrsmole.js
@@ -33,18 +33,26 @@ class OsrsmoleCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            description: filtered[2] || '\u200B',
+        };
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("⚔️ __**GIANT MOLE MASS**__ ⚔️")
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! Just follow the mole and hit her. That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://oldschool.runescape.wiki/images/f/fd/Giant_Mole.png?3f58a")
         .setThumbnail("https://oldschool.runescape.wiki/images/f/fd/Giant_Mole.png?3f58a")
-        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + "🌍 **World:** 523\n**Host:** " + message.author)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + "🌍 **World:** 523\n**Host:** " + message.author)
         .addField("\u200b", "[Strategies for Giant mole](https://oldschool.runescape.wiki/w/Giant_Mole/Strategies)")
         .addField("\u200b", "**Requirements:**\nLantarn or Firecape \nSpade", true)
         .addField("\u200b", "**Recommended:**\nFood \nPrayer Potions", true)
         .addBlankField()
-        .addField("\u200b", args.description, true)
+        .addField("\u200b", new_args.description, true)
         message.channel.sendEmbed(myInfo);
     }
 }
