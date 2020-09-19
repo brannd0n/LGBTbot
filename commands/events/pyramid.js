fb60f06ad4888d7e396000c09fb90b8013b9be12
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-18 21:25:33 -0500

    Update pyramid.js

--- commands/events/pyramid.js
+++ commands/events/pyramid.js
@@ -33,14 +33,22 @@ class PyramidCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            description: filtered[3] || '\u200B',
+        };
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("__**Pyramid Plunder**__")
         .setColor(0x4F2095)
         .setFooter("Please remember that this is completely for fun!", "https://oldschool.runescape.wiki/images/0/01/Pyramid_Plunder_logo.jpg?70655")
         .setThumbnail("https://oldschool.runescape.wiki/images/0/01/Pyramid_Plunder_logo.jpg?70655")
-        .addField("\u200b","**Date:** " + args.date + "\n**Time:** " + args.time + "\n**Host: **" + message.author +"\n**World:** W523")
-        .addField("\u200b", args.description, true)
+        .addField("\u200b","**Date:** " + new_args.date + "\n**Time:** " + new_args.time + "\n**Host: **" + message.author +"\n**World:** W523")
+        .addField("\u200b", new_args.description, true)
         message.channel.send(myInfo);
     }
 }
