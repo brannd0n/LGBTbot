f7e1c1cb18d486379b17d45c63548a1087bc3164
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-18 19:14:41 -0500

    Update nex.js

--- commands/events/nex.js
+++ commands/events/nex.js
@@ -32,17 +32,25 @@ class NexCommand extends commando.Command
 
     async run(message, args)
     {
+        const arg_string = Object.values(args).map(val => val);
+        const updated_args = arg_string.join(' ').split(']').join(' ').split('[').slice(1);
+        const filtered = updated_args.filter(el => el && el != '');
+        const new_args = {
+            date: filtered[0] || '',
+            time:  filtered[1] || '',
+            description: filtered[2] || '\u200B',
+        };
         message.delete();
         var myInfo = new discord.RichEmbed()
         .setTitle("⚔️ __**NEX MASS**__ ⚔️")
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! If you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/thumb/f/fe/Nex.png/500px-Nex.png?67c8a")
         .setThumbnail("https://runescape.wiki/images/thumb/f/fe/Nex.png/500px-Nex.png?67c8a")
-        .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + message.author)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + message.author)
         .addField("\u200b", "[Strategies for Nex](https://runescape.wiki/w/Nex/Strategies)\nPlease try not to bleed Nex during blood phase.")
         .addField("\u200b", "**Requirements:**\nA charged frozen key\nEither 40 Zaros kills or Ancient ceremonial robes", true)
         .addField("\u200b", "**Recommended:**\nTwo combat styles \nFood \nPrayer Potions \nA trip to glacor caves", true)
-        .addField("\u200b",args.description, true)
+        .addField("\u200b",new_args.description, true)
         message.channel.sendEmbed(myInfo);
     }
 }
