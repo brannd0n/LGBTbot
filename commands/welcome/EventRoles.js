de0907c14a5193ff5946fbf6e59d1551ae66d502
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-09-20 09:19:03 -0500

    Update EventRoles.js

--- commands/welcome/EventRoles.js
+++ commands/welcome/EventRoles.js
@@ -19,7 +19,9 @@ class EventRolesCommand extends commando.Command
         .addField("__Event Roles__", "While in this server everyone has the ability to self assign events roles to be tagged for events hosted on RS3 and OSRS.\n\n" +
         "To assign yourself the event role react to the following and you will be given the role.\n\n" +
         ":heart: - <@&635078700660031518>\n" +
-        ":green_heart: - <@&669623198224220170>\n")
+        ":green_heart: - <@&669623198224220170>\n" +
+        ":blue_heart: - <@&757214043579744346>\n" +
+        ":purple_heart: - <@&757214331703394365>\n")
         .setColor(0x00FFFF)
         .setFooter("Last updated")
         .setTimestamp();
