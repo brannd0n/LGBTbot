bb5bb3d4da5dee564d0f6c72f9ec66910a25f072
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-10-29 19:20:57 -0500

    Update clanniemole.js

--- commands/events/clanniemole.js
+++ commands/events/clanniemole.js
@@ -54,7 +54,7 @@ class ClannieMoleCommand extends commando.Command
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! Just follow the mole and hit her. That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/thumb/f/fd/Giant_Mole.png/240px-Giant_Mole.png?0e85e")
         .setThumbnail("https://runescape.wiki/images/thumb/f/fd/Giant_Mole.png/240px-Giant_Mole.png?0e85e")
-        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + "<@!"+  new_args.host.id +">"t)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + "<@!"+  new_args.host.id +">")
         .addField("\u200b", "[Strategies for Giant mole](https://runescape.wiki/w/Giant_mole/Strategies)")
         .addField("\u200b", "**Requirements:**\nNone", true)
         .addField("\u200b", "**Recommended:**\nFood \n Prayer Potions", true)
