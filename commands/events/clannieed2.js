8c2fbcebd0111a91599caf1837fa5a2dc3a58273
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-10-30 09:13:24 -0500

    Update clannieed2.js

--- commands/events/clannieed2.js
+++ commands/events/clannieed2.js
@@ -52,7 +52,7 @@ class ClannieEd2Command extends commando.Command
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! Please feel free to PM me. All loot will be traded to me and split equally between attendees.", "https://runescape.wiki/images/thumb/a/a3/Black_stone_dragon.png/1200px-Black_stone_dragon.png?be0ef")
         .setThumbnail("https://runescape.wiki/images/thumb/a/a3/Black_stone_dragon.png/1200px-Black_stone_dragon.png?be0ef")
-        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + "<@!"+  new_args.host.id +">")
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + new_args.host)
         .addField("\u200b", "[Strategies for Dragonkin Laboratory](https://runescape.wiki/w/Dragonkin_Laboratory/Strategies)")
         .addField("\u200b", "**Requirements:**\nAt least Tier 70 weapon and armour \nSuper Antifires or Reg Antifires \nBeast of Burden \nAt least 43 Prayer", true)
         .addField("\u200b", "**Recommended:**\nTier 80 weapons or higher \n Supreme Overload Salves \nMobile Perk \nRing of Death", true)
