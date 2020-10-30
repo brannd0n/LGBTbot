81580e3a8fadeeb30576d66e46d1eb48e55b1e1a
brannd0n <52438429+brannd0n@users.noreply.github.com>
2020-10-29 19:21:58 -0500

    Update clannietwins.js

--- commands/events/clannietwins.js
+++ commands/events/clannietwins.js
@@ -47,7 +47,7 @@ class ClannieTwinsCommand extends commando.Command
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! Please get your kill count before the event start time, you keep your kc even though you leave GWD2! Don't get charged, and stay out of the dangerous floor tiles! That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/7/72/Nymora_chathead.png?e37c4")
         .setThumbnail("https://runescape.wiki/images/6/6b/Avaryss%2C_the_Unceasing.png?097c7")
-        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + "<@!"+  new_args.host.id +">"t)
+        .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + "<@!"+  new_args.host.id +">")
         .addField("\u200b", "[Strategies for Twin Furies (wiki)](https://runescape.wiki/w/Twin_Furies/Strategies)")
         .addField("\u200b", "**Requirements:**\n80 Ranged\n40 Zamorak kill count", true)
         .addField("\u200b", "**Recommended:**\n70+ Prayer (for piety)\n90+ Herb for extremes\nAnything above barrows armour\nAny style works \nHeart tele tabs \nBoB filled with food.", true)
