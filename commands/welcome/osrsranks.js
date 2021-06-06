632c7c70810ba3b1078378993cf2b7f3b9a9367a
brannd0n <52438429+brannd0n@users.noreply.github.com>
2021-06-05 22:24:42 -0500

    Update osrsranks.js

--- commands/welcome/osrsranks.js
+++ commands/welcome/osrsranks.js
@@ -20,7 +20,7 @@ class OsrsranksCommand extends commando.Command
         .setFooter("Last updated")
         .setTimestamp()
         .addField("🗨️ __OSRS Clan__ 🗨️", "This is the official ranking system in place for our OSRS faction of the Clan. As we are a social and community clan, the ranks are based on length of time in the Clan. The amount of time you need to be in the Clan for each rank is as follows:")
-        .addField("😊 __Member Ranks__ 😊", ":medic: Brand new clannie! \n \n :pure: 3 weeks of being a member \n \n :champion: 3 months of being a member \n \n :epic: 6 months of being a member \n \n :mystic: 1 year of being a member \n \n :hero: 2 years of being a member")
+        .addField("😊 __Member Ranks__ 😊", "<:Medic:850932297113010257> Brand new clannie! \n \n <:Pure:850931886159298570> 3 weeks of being a member \n \n <:Champion:850932422131580948> 3 months of being a member \n \n <:Epic:850932396140396544> 6 months of being a member \n \n <:Mystic:850932445401841675> 1 year of being a member \n \n <:Hero:850932477978738738> 2 years of being a member")
         .addField(" \u200b ", "**:earth_americas: Clan World is 523 :earth_africa:**")
         message.channel.send(myInfo)
     }
