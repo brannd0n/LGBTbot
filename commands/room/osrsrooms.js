db4dbb1d8078b4099baaa41ba384ab6c2eede891
brannd0n <52438429+brannd0n@users.noreply.github.com>
2021-06-11 16:30:00 -0500

    Update osrsrooms.js

--- commands/room/osrsrooms.js
+++ commands/room/osrsrooms.js
@@ -18,7 +18,7 @@ class OsrsRoomCommand extends commando.Command
         var myInfo = new discord.MessageEmbed()
         .setTitle("OSRS Rooms")
         .setColor(0xFBD700)
-        .setDescription("<#532710360919310347> - 'osrs lgbt' fc. Discuss anything related to old school runescape here.  \n \n <#664616078705426452> - All the FC events will be announced and discussed here! \n \n <#666743386417528862> - All OSRS game and clan announcements \n \n <#701441756839018576> - Share in-game screenshots of outfits, gameplay and events. Also for discussion and input on in-game outfits. \n \n <#664616138638098432> - Post all your OSRS drops, clue rewards, skill achievements, quests completed and goals achieved here! \n \n <#672124924336275479> - Use this room for helpful advice on OSRS bosses, clues, quests, etc. \n \n <#729120469705424977> - Please use this room for all osrs related bot commands. \n \n " )
+        .setDescription("<#532710360919310347> - Discuss anything related to old school runescape here.  \n \n <#664616078705426452> - All the FC events will be announced and discussed here! \n \n <#666743386417528862> - All OSRS game and clan announcements \n \n <#701441756839018576> - Share in-game screenshots of outfits, gameplay and events. Also for discussion and input on in-game outfits. \n \n <#664616138638098432> - Post all your OSRS drops, clue rewards, skill achievements, quests completed and goals achieved here! \n \n <#672124924336275479> - Use this room for helpful advice on OSRS bosses, clues, quests, etc. \n \n <#729120469705424977> - Please use this room for all osrs related bot commands. \n \n " )
         .setFooter("Last updated")
         .setTimestamp();
         message.channel.send(myInfo)
