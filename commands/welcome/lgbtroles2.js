7c246b60c4f1877754b432761a8ba26e6c1f919c
brannd0n <52438429+brannd0n@users.noreply.github.com>
2021-06-27 20:23:47 -0500

    Update lgbtroles2.js

--- commands/welcome/lgbtroles2.js
+++ commands/welcome/lgbtroles2.js
@@ -16,7 +16,7 @@ class Lgbtroles2Command extends commando.Command
     async run(message, args)
     {
         var myInfo = new discord.MessageEmbed()
-        .setColor(0x22FF00)
+        .setColor(0xFF00FF)
         .setFooter("Last updated")
         .setTimestamp()
         .addField("**LGBT+ Roles Continued:**", "Biromantic - <:Biromantic:858795120344956939> \n \nAll The Things - <:lgbt:760240140894994513> \n \nNon-Binary - <:non_binary:760239301639405568> \n \nGenderfluid - <:gender_fluid:760240032992329739>  \n \nReciprosexual - <:Reciprosexual:760271871777832960> \n \nAgender - <:agender:760239950909669420> \n \nQueer - <:Queer_Heart:760323391331762258> \n \nNeptunic - <:Neptunic:761644159660064809> \n \nSaturnic - <:Saturnic:761644352605913118> \n \nUranic - <:Uranic:761644473985794070>")
