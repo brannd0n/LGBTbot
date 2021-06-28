5e96d46c47ee3c6b729895c715afdffb1248b4db
brannd0n <52438429+brannd0n@users.noreply.github.com>
2021-06-27 20:25:02 -0500

    Update OtherRoles.js

--- commands/welcome/OtherRoles.js
+++ commands/welcome/OtherRoles.js
@@ -16,7 +16,7 @@ class OtherrolesCommand extends commando.Command
     async run(message, args)
     {
         var myInfo = new discord.MessageEmbed()
-        .setColor(0x22FF00)
+        .setColor(0xFF9200)
         .setFooter("Last updated")
         .setTimestamp()
         .addField("**Other Roles:**", "Pokemon Go - <:pokeball:760303716871438336> \n \nNintendo - <:Nintendo:760304615073120276> \n \nXbox - <:Xbox:760303353456099328> \n \nPlaystation - <:Playstation:760306144186466345>\n \nSteam - <:Steam:760303410883854336> \n \nPC - <:PC:760305504936263681>\n \nMovie Night - 🎥 \n \nMuggle - <:muggle:760272621878509568> \n \nGryffindor - <:Gryffindor:305505772438028288> \n \nHufflepuff - <:hufflepuff:306825962140598272>\n \nRavenclaw - <:ravenclaw:346329202951192586> \n \nSlytherin - <:slytherin:305498125936885760> \n \nClan Newsletter - 🗞️")
