a7f19315271cce9eb6bb82c49ba4bb28172ed0a4
brannd0n <52438429+brannd0n@users.noreply.github.com>
2021-11-23 16:54:59 -0600

    Update clannievorago.js

--- commands/events/clannievorago.js
+++ commands/events/clannievorago.js
@@ -1,15 +1,15 @@
 const commando = require('discord.js-commando');
 const discord = require('discord.js');
 
-class ClannieVoragoCommand extends commando.Command
+class ClannieVinnyCommand extends commando.Command
 {
     constructor(client)
     {
         super(client,{
-            name: 'clannievorago',
+            name: 'clannievinny',
             group: 'events',
-            memberName: 'clannievorago',
-            description: 'Vorago event details',
+            memberName: 'clannievinny',
+            description: 'Vindicta event details',
             args: [
                 {
                     key: 'date',
@@ -20,20 +20,20 @@ class ClannieVoragoCommand extends commando.Command
                     key: 'time',
                     prompt: 'What time is the event? (e.g. 21:00)',
                     type: 'string'
-                  },
-                  {
+                },
+                {
                     key: 'host',
                     prompt: 'Who is the host?',
                     type: 'string'
                 },
-                  {
-                      key: 'description',
-                      prompt: 'Describe the event',
-                      type: 'string'
-                  }
-              ]
-          });
-      }
+                {
+                    key: 'description',
+                    prompt: 'Describe the event',
+                    type: 'string'
+                }
+            ]
+        });
+    }
 
     async run(message, args)
     {
@@ -46,20 +46,19 @@ class ClannieVoragoCommand extends commando.Command
             host:  filtered[2] || '',
             description: filtered[3] || '',
         };
-        
         message.delete();
         var myInfo = new discord.MessageEmbed()
-        .setTitle("⚔️ __**VORAGO MASS**__ ⚔️")
+        .setTitle("⚔️ __**VINDICTA MASS**__ ⚔️")
         .setColor(0x00AE86)
-        .setFooter("Please remember that this is completely for fun! We will be voice chatting during this mass to give out calls and such. If you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/thumb/e/eb/Vorago.png/753px-Vorago.png?d1cfb")
-        .setThumbnail("https://runescape.wiki/images/thumb/e/eb/Vorago.png/753px-Vorago.png?d1cfb")
+        .setFooter("Please remember that this is completely for fun! Please get your kill count before the event start time, you keep your kc even though you leave GWD2! Keep moving! Don't stand in the flames! That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://vignette.wikia.nocookie.net/runescape2/images/4/40/Gorvek.png/revision/latest/scale-to-width-down/200?cb=20170517031043")
+        .setThumbnail("https://runescape.wiki/images/thumb/4/40/Gorvek.png/247px-Gorvek.png")
         .addField("\u200b","📅 **Date:** " + new_args.date + "\n🕘 **Time:** " + new_args.time + " game-time\n🌍 **World:** 23\n**Host:** " + new_args.host)
-        .addField("\u200b", "[Strategies for Vorago (wiki)](https://runescape.wiki/w/Vorago/Strategies)")
-        .addField("\u200b", "**Requirements:**\nTalk to Vorago", true)
-        .addField("\u200b", "**Recommended:**\nAncient Curses\nOverloads\nT80+ DPS Armour\nMagic or Ranged \nBoB filled with food.", true)
+        .addField("\u200b", "[Strategies for Vindicta (wiki)](https://runescape.wiki/w/Vindicta/Strategies)")
+        .addField("\u200b", "**Requirements:**\n80 Attack\n40 Zaros kill count", true)
+        .addField("\u200b", "**Recommended:**\n70+ Prayer (for piety)\n90+ Herb for extremes\nAnything above barrows armour\nAny style works \nHeart tele tabs \nBoB filled with food.", true)
         .addField("\u200b", new_args.description, true);
         message.channel.send(myInfo)
     }
 }
 
-module.exports = ClannieVoragoCommand;
+module.exports = ClannieVinnyCommand;
