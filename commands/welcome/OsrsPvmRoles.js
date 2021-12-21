8ad35801669545bfa78653d52dbf4e2a36181c34
brannd0n <52438429+brannd0n@users.noreply.github.com>
2021-12-20 21:43:54 -0600

    Update OsrsPvmRoles.js

--- commands/welcome/OsrsPvmRoles.js
+++ commands/welcome/OsrsPvmRoles.js
@@ -19,7 +19,7 @@ class OsrspvmrolesCommand extends commando.Command
         .setColor(0x22FF00)
         .setFooter("Last updated")
         .setTimestamp()
-        .addField("**OSRS PVM Roles:**", "Nex - <:osrs_nex:922521370754183268> \n \nBandos - <:osrs_bandos:922521155787714641> \n \nSaradomin - <:osrs_sara:922519534143930418> \n \nArmadyl - <:armadyl:597892443077214211> \n \nZamorak - <:osrs_zamorak:922521155905159198> \n \nDagannoth Kings - <:osrs_dks:922523717593096302>\n \nCorporeal Beast - <:osrs_corp:922523620285223023>  \n \nCox - <:osrs_cox:922523197285498940> \n \nToB - <:osrs_tob:922523397945176104> ")
+        .addField("**OSRS PVM Roles:**", "Nex - <:osrs_nex:922521370754183268> \n \nBandos - <:osrs_bandos:922521155787714641> \n \nSaradomin - <:osrs_sara:922519534143930418> \n \nArmadyl - <:osrs_armadyl:922521155900948530> \n \nZamorak - <:osrs_zamorak:922521155905159198> \n \nDagannoth Kings - <:osrs_dks:922523717593096302>\n \nCorporeal Beast - <:osrs_corp:922523620285223023>  \n \nCox - <:osrs_cox:922523197285498940> \n \nToB - <:osrs_tob:922523397945176104> ")
         message.channel.send(myInfo)
     }
 }
