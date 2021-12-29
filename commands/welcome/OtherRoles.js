764466f6c7e08ab37d7a108a328a1944252130c4
brannd0n <52438429+brannd0n@users.noreply.github.com>
2021-12-29 11:38:36 -0600

    Update OtherRoles.js

--- commands/welcome/OtherRoles.js
+++ commands/welcome/OtherRoles.js
@@ -19,7 +19,7 @@ class OtherrolesCommand extends commando.Command
         .setColor(0xFF9200)
         .setFooter("Last updated")
         .setTimestamp()
-        .addField("**Other Roles:**", "Pokemon Go - <:pokeball:760303716871438336> \n \nNintendo - <:Nintendo:760304615073120276> \n \nXbox - <:Xbox:760303353456099328> \n \nPlaystation - <:Playstation:760306144186466345>\n \nSteam - <:Steam:760303410883854336> \n \nPC - <:PC:760305504936263681>\n \nMovie Night - 🎥 \n \nMuggle - <:muggle:760272621878509568> \n \nGryffindor - <:Gryffindor:305505772438028288> \n \nHufflepuff - <:hufflepuff:306825962140598272>\n \nRavenclaw - <:ravenclaw:346329202951192586> \n \nSlytherin - <:slytherin:305498125936885760> \n \nClan Newsletter - 🗞️")
+        .addField("**Other Roles:**", "Pokemon Go - <:pokeball:760303716871438336> \n \nNintendo - <:Nintendo:760304615073120276> \n \nXbox - <:Xbox:760303353456099328> \n \nPlaystation - <:Playstation:760306144186466345>\n \nSteam - <:Steam:760303410883854336> \n \nPC - <:PC:760305504936263681>\n \nMinecraft - <a:minecraft:925803557272297522>\n \nMovie Night - 🎥 \n \nMuggle - <:muggle:760272621878509568> \n \nGryffindor - <:Gryffindor:305505772438028288> \n \nHufflepuff - <:hufflepuff:306825962140598272>\n \nRavenclaw - <:ravenclaw:346329202951192586> \n \nSlytherin - <:slytherin:305498125936885760> \n \nClan Newsletter - 🗞️")
         message.channel.send(myInfo)
     }
 }
