3271a08b35f04b861b14cd8d56bfe514bb3a3265
brannd0n <52438429+brannd0n@users.noreply.github.com>
2022-06-01 23:32:50 -0500

    Update Rs3PvmRoles.js

--- commands/welcome/Rs3PvmRoles.js
+++ commands/welcome/Rs3PvmRoles.js
@@ -19,7 +19,7 @@ class Rs3pvmrolesCommand extends commando.Command
         .setColor(0x22FF00)
         .setFooter("Last updated")
         .setTimestamp()
-        .addField("**RS3 PVM Roles:**", "AOD - <:AoD:807576688551919628> \n \nED1 - 1️⃣ \n \nED2 - 2️⃣ \n \nED3 - 3️⃣ \n \nMazcab Raids - <:Beastmaster:858833521463656478> \n \nReaper - 💀\n \nNex - <:Nex:858833978156646440>  \n \nGWD2 - <:GWD2:858834522891485224> \n \nGWD3 - ⚔️\n\nRaksha - <:Raksha:858835233951842324> \n \nRotS - :six:")
+        .addField("**RS3 PVM Roles:**", "AOD - <:AoD:807576688551919628> \n \nED1 - 1️⃣ \n \nED2 - 2️⃣ \n \nED3 - 3️⃣ \n \nMazcab Raids - <:Beastmaster:858833521463656478> \n \nReaper - 💀\n \nNex - <:Nex:858833978156646440>  \n \nGWD2 - <:GWD2:858834522891485224> \n \nGWD3 - ⚔️\n\nRaksha - <:Raksha:858835233951842324> \n \nRotS - :six: \n \nDungeoneering - <:dungeoneering:981745426388643850>")
         message.channel.send(myInfo)
     }
 }
