f8946a6d7bfa98d5778af9b897d6816c456984a8
brannd0n <52438429+brannd0n@users.noreply.github.com>
2022-06-01 23:20:42 -0500

    Update OsrsPvmRoles.js

--- commands/welcome/OsrsPvmRoles.js
+++ commands/welcome/OsrsPvmRoles.js
@@ -16,7 +16,7 @@ class OsrspvmrolesCommand extends commando.Command
     async run(message, args)
     {
         var myInfo = new discord.MessageEmbed()
-        .setColor(0x22FF00)
+        .setColor(0xFF8A00)
         .setFooter("Last updated")
         .setTimestamp()
         .addField("**OSRS PVM Roles:**", "Nex - <:nex:933671455273676861> \n \nBandos - <:bandos:933671513494810625> \n \nSaradomin - <:saradomin:933671513972936704> \n \nArmadyl - <:armadyl:933671513821950003> \n \nZamorak - <:zamorak:933671514098790410> \n \nDagannoth Kings - <:osrs_dks:922523717593096302>\n \nCorporeal Beast - <:corp:933671384490590219> \n \nCox - <:cox:933670425089695774> \n \nToB - <:tob:933671514111377508> \n \nBarbarian Assault - <:ba:922902213414748180> \n \nCox CM - <:cox_cm:922902671449542686> \n \nZalcano - <:zalcano:922902878614601818>")
